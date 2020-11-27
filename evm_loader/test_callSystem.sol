89232864864372349bc4c40289b284fc81f477fb
soft-bagel-93 <boostmultifruit@mail.ru>
2020-11-27 11:25:47 +0300

    Fix test contract for calls

--- evm_loader/test_callSystem.sol
+++ evm_loader/test_callSystem.sol
@@ -6,25 +6,28 @@ contract helloWorld {
         address name;
         bool is_signer;
         bool is_writable;
+        bool need_translate;
     }
+
+    address solana = 0xBd770416a3345F91E4B34576cb804a576fa48EB1;
 
     function testCall() public { 
         bool status;
         
-        address program_id = 0xBd770416a3345F91E4B34576cb804a576fa48EB1;
+        string memory program_id = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
         
         AccountMeta[] memory accs = new AccountMeta[](2);
-        accs[0] = AccountMeta(0xBd770416a3345F91E4B34576cb804a576fa48EB1, true, false);
-        accs[1] = AccountMeta(address(this), false, true);
+        accs[0] = AccountMeta(0xBd770416a3345F91E4B34576cb804a576fa48EB1, true, false, true);
+        accs[1] = AccountMeta(address(this), false, true, true);
         
         bytes memory instruction_data = hex"7472616E7366657228Bd770416a3345F91E4B34576cb804a576fa48EB1Bd770416a3345F91E4B34576cb804a576fa48EB1000000000000000029";
         
         bytes memory call_data = abi.encodeWithSignature(
-            "callExternal(address, (address, bool, bool)[], hex)",
+            "(string, (address, bool, bool, bool)[], hex)",
             program_id, accs, instruction_data);
 
         bytes memory result;
-        (status, result) = address(this).call(call_data);
+        (status, result) = solana.call(call_data);
         if (!status) {
             revert();
         }
