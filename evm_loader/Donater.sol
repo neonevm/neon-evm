9fefb1d8c2b6519a7ccc9d9a8fa36f7d56df458e
soft-bagel-93 <boostmultifruit@mail.ru>
2020-12-16 13:13:31 +0300

    Fix Donater contract

--- evm_loader/Donater.sol
+++ evm_loader/Donater.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.5.12;
 import './ERC20Wrapper.sol';
 
 contract Donater {
-    function donate() public {
-        ERC20Wrapper e = ERC20Wrapper(address(this));
+    function donate(address wrapper) public {
+        ERC20Wrapper e = ERC20Wrapper(wrapper);
         return e.transfer(true, hex"0000000000000000000000000000000000000001", 5);
     }
     
-    function donateFrom() public {
-        ERC20Wrapper e = ERC20Wrapper(address(this));
+    function donateFrom(address wrapper) public {
+        ERC20Wrapper e = ERC20Wrapper(wrapper);
         return e.transferFrom(
             true, hex"0000000000000000000000000000000000000002",
             true, hex"0000000000000000000000000000000000000001", 5);
