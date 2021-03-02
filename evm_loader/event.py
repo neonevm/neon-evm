dc92f332e028a6175022b196a46b5a5c4e4c1db9
sinev-valentine <sinev-valentine@yandex.ru>
2021-03-02 13:09:44 +0300

    add test of different instructions' events

--- evm_loader/event.py
+++ evm_loader/event.py
@@ -29,6 +29,7 @@ class EventTest(unittest.TestCase):
         if getBalance(cls.caller) == 0:
             print("Create caller account...")
             _ = cls.loader.createEtherAccount(cls.caller_ether)
+            print("Done\n")
 
         print('Account:', cls.acc.public_key(), bytes(cls.acc.public_key()).hex())
         print("Caller:", cls.caller_ether.hex(), cls.caller_nonce, "->", cls.caller,
@@ -38,25 +39,8 @@ class EventTest(unittest.TestCase):
         print ('contract', cls.reId)
         print ('contract_eth', cls.reId_eth.hex())
 
-    def call_signed(self, input, raw_result):
-        tx = {
-            'to': solana2ether(self.reId),
-            'value': 1,
-            'gas': 1,
-            'gasPrice': 1,
-            'nonce': getTransactionCount(http_client, self.caller),
-            'data': input,
-            'chainId': 1
-        }
-        (from_addr, sign, msg) = make_instruction_data_from_tx(tx, self.acc.secret_key())
-        assert (from_addr == self.caller_ether)
-        keccak_instruction = make_keccak_instruction_data(1, len(msg))
-
-        evm_instruction = from_addr + sign + msg
-        trx = Transaction().add(
-            TransactionInstruction(program_id=keccakprog, data=keccak_instruction, keys=[
-                AccountMeta(pubkey=PublicKey(keccakprog), is_signer=False, is_writable=False), ])).add(
-            TransactionInstruction(program_id=evm_loader_id,
+    def sol_instr_05(self, evm_instruction):
+        return TransactionInstruction(program_id=evm_loader_id,
                                    data=bytearray.fromhex("05") + evm_instruction,
                                    keys=[
                                        AccountMeta(pubkey=self.reId, is_signer=False, is_writable=True),
@@ -64,23 +48,28 @@ class EventTest(unittest.TestCase):
                                        AccountMeta(pubkey=PublicKey(sysinstruct), is_signer=False, is_writable=False),
                                        AccountMeta(pubkey=evm_loader_id, is_signer=False, is_writable=True),
                                        AccountMeta(pubkey=PublicKey(sysvarclock), is_signer=False, is_writable=False),
-                                   ]))
+                                   ])
 
-        result = http_client.send_transaction(trx, self.acc,
+    def sol_instr_keccak(self, keccak_instruction):
+        return TransactionInstruction(program_id=keccakprog, data=keccak_instruction, keys=[
+                AccountMeta(pubkey=PublicKey(keccakprog), is_signer=False, is_writable=False), ])
+
+    def call_signed(self, input):
+        tx = {'to': solana2ether(self.reId), 'value': 1, 'gas': 1, 'gasPrice': 1,
+            'nonce': getTransactionCount(http_client, self.caller), 'data': input, 'chainId': 1}
+
+        (from_addr, sign, msg) = make_instruction_data_from_tx(tx, self.acc.secret_key())
+        assert (from_addr == self.caller_ether)
+        trx = Transaction()
+        trx.add(self.sol_instr_keccak(make_keccak_instruction_data(1, len(msg))))
+        trx.add(self.sol_instr_05(from_addr + sign + msg))
+        return http_client.send_transaction(trx, self.acc,
                                      opts=TxOpts(skip_confirmation=False, preflight_commitment="root"))["result"]
-        if raw_result:
-            return result
-        messages = result["meta"]["logMessages"]
-        res = messages[messages.index("Program log: succeed") + 1]
-        if not res.startswith("Program log: "):
-            raise Exception("Invalid program logs: no result")
-        else:
-            return bytearray.fromhex(res[13:])
 
     def test_addNoReturn(self):
         func_name = abi.function_signature_to_4byte_selector('addNoReturn(uint8,uint8)')
         data = (func_name + bytes.fromhex("%064x" % 0x1) + bytes.fromhex("%064x" % 0x2) )
-        result = self.call_signed(input=data, raw_result=True)
+        result = self.call_signed(input=data)
         self.assertEqual(result['meta']['err'], None)
         self.assertEqual(len(result['meta']['innerInstructions']), 1)
         self.assertEqual(len(result['meta']['innerInstructions'][0]['instructions']), 1)
@@ -91,7 +80,7 @@ class EventTest(unittest.TestCase):
     def test_addReturn(self):
         func_name = abi.function_signature_to_4byte_selector('addReturn(uint8,uint8)')
         data = (func_name + bytes.fromhex("%064x" % 0x1) + bytes.fromhex("%064x" % 0x2))
-        result = self.call_signed(input=data, raw_result=True)
+        result = self.call_signed(input=data)
         self.assertEqual(result['meta']['err'], None)
         self.assertEqual(len(result['meta']['innerInstructions']), 1)
         self.assertEqual(len(result['meta']['innerInstructions'][0]['instructions']), 1)
@@ -103,7 +92,7 @@ class EventTest(unittest.TestCase):
     def test_addReturnEvent(self):
         func_name = abi.function_signature_to_4byte_selector('addReturnEvent(uint8,uint8)')
         data = (func_name + bytes.fromhex("%064x" % 0x1) + bytes.fromhex("%064x" % 0x2))
-        result = self.call_signed(input=data, raw_result=True)
+        result = self.call_signed(input=data)
         self.assertEqual(result['meta']['err'], None)
         self.assertEqual(len(result['meta']['innerInstructions']), 1)
         self.assertEqual(result['meta']['innerInstructions'][0]['index'], 1)  # second instruction
@@ -121,24 +110,89 @@ class EventTest(unittest.TestCase):
     def test_addReturnEventTwice(self):
         func_name = abi.function_signature_to_4byte_selector('addReturnEventTwice(uint8,uint8)')
         data = (func_name + bytes.fromhex("%064x" % 0x1) + bytes.fromhex("%064x" % 0x2))
-        result = self.call_signed(input=data, raw_result=True)
+        result = self.call_signed(input=data)
         self.assertEqual(result['meta']['err'], None)
         self.assertEqual(len(result['meta']['innerInstructions']), 1)
         self.assertEqual(result['meta']['innerInstructions'][0]['index'], 1)  # second instruction
         self.assertEqual(len(result['meta']['innerInstructions'][0]['instructions']), 3)
         data = b58decode(result['meta']['innerInstructions'][0]['instructions'][0]['data'])
+        # self.assertEqual(data[:1], b'\x07')
         self.assertEqual(data[1:21], self.reId_eth)
         self.assertEqual(data[21:29], bytes().fromhex('%016x' % 1)[::-1])  # topics len
         self.assertEqual(data[29:61], abi.event_signature_to_log_topic('Added(uint8)'))  #topics
         self.assertEqual(data[61:93], bytes().fromhex("%064x" % 0x3))  # sum
         data = b58decode(result['meta']['innerInstructions'][0]['instructions'][1]['data'])
+        self.assertEqual(data[:1], b'\x07')  # 7 means OnEvent
         self.assertEqual(data[1:21], self.reId_eth)
         self.assertEqual(data[21:29], bytes().fromhex('%016x' % 1)[::-1])  # topics len
         self.assertEqual(data[29:61], abi.event_signature_to_log_topic('Added(uint8)'))  #topics
         self.assertEqual(data[61:93], bytes().fromhex("%064x" % 0x5))  # sum
         data = b58decode(result['meta']['innerInstructions'][0]['instructions'][2]['data'])
         self.assertEqual(data[:1], b'\x06')   # 6 means OnReturn
-        self.assertEqual(data[1:33], bytes().fromhex('%064x' % 3)) #sum
+        self.assertEqual(data[1:33], bytes().fromhex('%064x' % 5)) #sum
+
+    def test_events_of_different_instructions(self):
+        func_name = abi.function_signature_to_4byte_selector('addReturnEventTwice(uint8,uint8)')
+        input1 = (func_name + bytes.fromhex("%064x" % 0x1) + bytes.fromhex("%064x" % 0x2))
+        input2 = (func_name + bytes.fromhex("%064x" % 0x3) + bytes.fromhex("%064x" % 0x4))
+        tx1 =  {'to': solana2ether(self.reId), 'value': 1, 'gas': 1, 'gasPrice': 1,
+            'nonce': getTransactionCount(http_client, self.caller), 'data': input1, 'chainId': 1}
+        tx2 =  {'to': solana2ether(self.reId), 'value': 1, 'gas': 1, 'gasPrice': 1,
+            'nonce': getTransactionCount(http_client, self.caller)+1, 'data': input2, 'chainId': 1}
+
+        (from_addr1, sign1, msg1) = make_instruction_data_from_tx(tx1, self.acc.secret_key())
+        (from_addr2, sign2, msg2) = make_instruction_data_from_tx(tx2, self.acc.secret_key())
+        assert (from_addr1 == self.caller_ether)
+        assert (from_addr2 == self.caller_ether)
+
+        trx = Transaction()
+        trx.add(self.sol_instr_keccak(make_keccak_instruction_data(1, len(msg1))))
+        trx.add(self.sol_instr_05(from_addr1 + sign1 + msg1))
+        trx.add(self.sol_instr_keccak(make_keccak_instruction_data(1, len(msg2))))
+        trx.add(self.sol_instr_05(from_addr2 + sign2 + msg2))
+        result = http_client.send_transaction(trx, self.acc,
+                                     opts=TxOpts(skip_confirmation=False, preflight_commitment="root"))["result"]
+        self.assertEqual(result['meta']['err'], None)
+        self.assertEqual(len(result['meta']['innerInstructions']), 2) # two transaction-instructions contain events and return_value
+
+        self.assertEqual(result['meta']['innerInstructions'][0]['index'], 1)  # second instruction
+        self.assertEqual(result['meta']['innerInstructions'][1]['index'], 3)  # second instruction
+
+        # log sol_instr_05(from_addr1 + sign1 + msg1)
+        self.assertEqual(len(result['meta']['innerInstructions'][0]['instructions']), 3)
+        data = b58decode(result['meta']['innerInstructions'][0]['instructions'][0]['data'])
+        self.assertEqual(data[:1], b'\x07')  # 7 means OnEvent
+        self.assertEqual(data[1:21], self.reId_eth)
+        self.assertEqual(data[21:29], bytes().fromhex('%016x' % 1)[::-1])  # topics len
+        self.assertEqual(data[29:61], abi.event_signature_to_log_topic('Added(uint8)'))  #topics
+        self.assertEqual(data[61:93], bytes().fromhex("%064x" % 0x3))  # sum
+        data = b58decode(result['meta']['innerInstructions'][0]['instructions'][1]['data'])
+        self.assertEqual(data[:1], b'\x07')  # 7 means OnEvent
+        self.assertEqual(data[1:21], self.reId_eth)
+        self.assertEqual(data[21:29], bytes().fromhex('%016x' % 1)[::-1])  # topics len
+        self.assertEqual(data[29:61], abi.event_signature_to_log_topic('Added(uint8)'))  #topics
+        self.assertEqual(data[61:93], bytes().fromhex("%064x" % 0x5))  # sum
+        data = b58decode(result['meta']['innerInstructions'][0]['instructions'][2]['data'])
+        self.assertEqual(data[:1], b'\x06')   # 6 means OnReturn
+        self.assertEqual(data[1:33], bytes().fromhex('%064x' % 0x5)) #sum
+
+        # log sol_instr_05(from_addr2 + sign2 + msg2)
+        self.assertEqual(len(result['meta']['innerInstructions'][1]['instructions']), 3)
+        data = b58decode(result['meta']['innerInstructions'][1]['instructions'][0]['data'])
+        self.assertEqual(data[:1], b'\x07')  # 7 means OnEvent
+        self.assertEqual(data[1:21], self.reId_eth)
+        self.assertEqual(data[21:29], bytes().fromhex('%016x' % 1)[::-1])  # topics len
+        self.assertEqual(data[29:61], abi.event_signature_to_log_topic('Added(uint8)'))  #topics
+        self.assertEqual(data[61:93], bytes().fromhex("%064x" % 0x7))  # sum
+        data = b58decode(result['meta']['innerInstructions'][1]['instructions'][1]['data'])
+        self.assertEqual(data[:1], b'\x07')  # 7 means OnEvent
+        self.assertEqual(data[1:21], self.reId_eth)
+        self.assertEqual(data[21:29], bytes().fromhex('%016x' % 1)[::-1])  # topics len
+        self.assertEqual(data[29:61], abi.event_signature_to_log_topic('Added(uint8)'))  #topics
+        self.assertEqual(data[61:93], bytes().fromhex("%064x" % 0xb))  # sum
+        data = b58decode(result['meta']['innerInstructions'][1]['instructions'][2]['data'])
+        self.assertEqual(data[:1], b'\x06')   # 6 means OnReturn
+        self.assertEqual(data[1:33], bytes().fromhex('%064x' % 0xb)) #sum
 
 if __name__ == '__main__':
     unittest.main()
