5f0c61cff76b3b1237b819033be1fc5e6604469c
Dmitriy Borisenko <otselnik@yandex.ru>
2021-03-26 19:07:07 +0300

    fix debug_print

--- evm_loader/program/src/debug.rs
+++ evm_loader/program/src/debug.rs
@@ -4,6 +4,11 @@ macro_rules! debug_print {
     ($( $args:expr ),*) => { solana_sdk::info!( $( $args ),* ) }
 }
 
+#[cfg(not(feature = "evm_debug"))]
+macro_rules! debug_print {
+    ($( $args:expr ),*) => {}
+}
+
 #[cfg(feature = "default")]
 macro_rules! debug_print {
     ($( $args:expr ),*) => { eprintln!( "{}", $( $args ),* ) }
