ac4bfff2b852e506729a9ce3bf9a340604862d18
sinev-valentine <sinev-valentine@yandex.ru>
2021-04-21 14:30:43 +0300

    test nested_call update

--- evm_loader/nested_call.sol
+++ evm_loader/nested_call.sol
@@ -36,35 +36,37 @@ contract nested_call_Recover{
 }
 
 contract nested_call_Receiver {
-    event Received(address caller, uint amount, string message);
-    event rec(address ad);
+    event Foo(address caller, uint amount, string message);
+    event Response_recovery_signer(bool success, bytes a);
 
     function foo(string memory _message, uint _x) public payable returns (uint) {
-        emit Received(msg.sender, msg.value, _message);
-
+        emit Foo(msg.sender, msg.value, _message);
         return _x + 1;
     }
 
-    function recover (address recover_addr, bytes32 hash, bytes memory sig) public {
-        // emit rec(signer);
-        recover_addr.call(abi.encodeWithSignature("recovery_signer(bytes32,bytes)", hash, sig));
+    function recover (address recover_addr, bytes32 hash, bytes memory sig) public returns (bool) {
+        (bool success, bytes memory signer) = recover_addr.call(abi.encodeWithSignature("recovery_signer(bytes32,bytes)", hash, sig));
+        emit Response_recovery_signer(success, signer);
+        return true;
     }
 }
 
 contract nested_call_Caller {
-    event Response(bool success, bytes data);
+    event Result(bool success, bytes data);
 
     function callFoo(address payable receiver) public payable {
 
         (bool success, bytes memory data) = receiver.call(
-            abi.encodeWithSignature("foo(string,uint256)", "call foo", 123)
-        );
-
-        emit Response(success, data);
+                abi.encodeWithSignature("foo(string,uint256)", "call foo", 123)
+            );
+        emit Result(success, data);
     }
 
-    function callRecover(address receiver_addr, address recover_addr, bytes32 hash, bytes  memory signature) public {
-        receiver_addr.call ( abi.encodeWithSignature("recover(address,bytes32,bytes)",recover_addr, hash, signature));
+    function callRecover(address receiver_addr, address recover_addr, bytes32 hash, bytes memory signature) public {
+         (bool success, bytes memory res) = receiver_addr.call(
+                        abi.encodeWithSignature("recover(address,bytes32,bytes)",recover_addr, hash, signature)
+                    );
+         emit Result(success, res);
     }
 
 }
