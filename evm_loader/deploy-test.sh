3429256e21c1764685c83d6583729b09798b2268
sinev-valentine <sinev-valentine@yandex.ru>
2021-05-17 15:59:58 +0300

    replace solana-deploy to solana

--- evm_loader/deploy-test.sh
+++ evm_loader/deploy-test.sh
@@ -18,7 +18,7 @@ echo "Run tests for EVM Loader"
 # Parse deployed contract address from output of solana-cli:
 # Example output: `Program Id: 853qJy1Z8hfgHe194fVrYUbVDfx88ny7phSCHc481Fc6`
 # EVM_LOADER will be empty if the match fails.
-export EVM_LOADER=$(solana-deploy deploy evm_loader.so | sed '/Program Id: \([0-9A-Za-z]\+\)/,${s//\1/;b};s/^.*$//;$q1')
+export EVM_LOADER=$(solana deploy evm_loader.so | sed '/Program Id: \([0-9A-Za-z]\+\)/,${s//\1/;b};s/^.*$//;$q1')
 if [ ${#EVM_LOADER} -eq 0 ]; then
   echo  "EVM_LOADER is not deployed"
   exit 1
