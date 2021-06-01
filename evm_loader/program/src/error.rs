b9aeed779abb8d83a1c9884699d203331996f16d
Dmitriy Borisenko <otselnik@yandex.ru>
2021-06-01 21:13:32 +0300

    add error StorageAccountUninitialized to mark case when continue executed after finalization #36

---
+++ evm_loader/program/src/error.rs
@@ -0,0 +1,28 @@
+//! Error types
+
+use num_derive::FromPrimitive;
+use solana_program::{decode_error::DecodeError, program_error::ProgramError};
+use thiserror::Error;
+
+/// Errors that may be returned by the EVM Loader program.
+#[derive(Clone, Debug, Eq, Error, FromPrimitive, PartialEq)]
+pub enum EVMLoaderError {
+    /// Storage Account is uninitialized.
+    #[error("Storage Account is uninitialized")]
+    StorageAccountUninitialized,
+    /// SomeError.
+    #[error("Some error")]
+    SomeError,
+}
+
+impl From<EVMLoaderError> for ProgramError {
+    fn from(e: EVMLoaderError) -> Self {
+        ProgramError::Custom(0xff00ff00 + (e as u32))
+    }
+}
+
+impl<T> DecodeError<T> for EVMLoaderError {
+    fn type_of() -> &'static str {
+        "EVMLoaderError"
+    }
+}
