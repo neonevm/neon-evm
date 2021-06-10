e6b138deca63d80050dac1c32d4d98f517233da8
Vasiliy Zaznobin <vasiliy.zaznobin@cyber-core.dev>
2021-06-10 21:22:59 +0300

    Add class EtherContract

--- evm_loader/ERC20/test/test_erc20.py
+++ evm_loader/ERC20/test/test_erc20.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import base58
 import unittest
+from solana.rpc import types
 from eth_tx_utils import make_keccak_instruction_data, Trx, make_instruction_data_from_tx
 from eth_utils import abi
 from web3.auto import w3
@@ -72,6 +73,137 @@ def get_trx_input(from_addr, sign, msg, step_count, mode=Mode.NORMAL):
     }[mode]
     print('trx_input:', trx_input.hex())
     return trx_input
+
+
+class ExecuteMode(Enum):
+    SINGLE = 0
+    ITERATIVE = 1
+
+
+class EtherContract:
+    """Encapsulate all data to execute Ether transaction on Solana."""
+
+    def __init__(self, solana_wallet, evm_loader, ether_caller, contract_account, contract_code_account):
+        self.mode = ExecuteMode.SINGLE
+        self.solana_wallet = solana_wallet
+        self.evm_loader = evm_loader
+        self.ether_caller = ether_caller
+        (self.solana_ether_caller, self.solana_caller_nonce) = self.evm_loader.ether2program(self.ether_caller)
+        if getBalance(self.solana_ether_caller) == 0:
+            print("Create caller account...")
+            self.solana_ether_caller = self.evm_loader.createEtherAccount(self.ether_caller)
+            print("Caller account:", self.solana_ether_caller)
+
+        self.contract_account = contract_account
+        self.contract_code_account = contract_code_account
+        self.storage = None
+
+    def set_execute_mode(self, new_mode):
+        self.mode = ExecuteMode(new_mode)
+
+    def create_storage_account(self, seed):
+        if self.storage is not None:
+            return
+        self.storage = PublicKey(
+            sha256(bytes(self.solana_wallet.public_key())
+                   + bytes(seed, 'utf8')
+                   + bytes(PublicKey(self.evm_loader.loader_id))).digest())
+        print("Storage", self.storage)
+
+        if getBalance(self.storage) == 0:
+            trx = Transaction()
+            trx.add(createAccountWithSeed(self.solana_wallet.public_key(),
+                                          self.solana_wallet.public_key(),
+                                          seed, 10 ** 9, 128 * 1024,
+                                          PublicKey(evm_loader_id)))
+            send_transaction(client, trx, self.solana_wallet)
+
+    def create_instruction_data_from_tx(self, input_data):
+        caller_trx_cnt = getTransactionCount(client, self.solana_ether_caller)
+        trx_raw = {'to': solana2ether(self.contract_account),
+                   'value': 1, 'gas': 1, 'gasPrice': 1, 'nonce': caller_trx_cnt,
+                   'data': input_data, 'chainId': 111}
+        return make_instruction_data_from_tx(trx_raw, self.solana_wallet.secret_key())
+
+    def call3_single(self, input_data, account_metas=None) -> types.RPCResponse:
+        data = bytearray.fromhex("03") + input_data
+        print('call_single with data:', data.hex())
+        trx = Transaction()
+        trx.add(
+            TransactionInstruction(program_id=self.evm_loader.loader_id, data=data, keys=
+            [
+                AccountMeta(pubkey=self.contract_account, is_signer=False, is_writable=True),
+                AccountMeta(pubkey=self.contract_code_account, is_signer=False, is_writable=True),
+                AccountMeta(pubkey=self.solana_ether_caller, is_signer=False, is_writable=True),
+                AccountMeta(pubkey=self.solana_wallet.public_key(), is_signer=True, is_writable=False),
+                AccountMeta(pubkey=self.evm_loader.loader_id, is_signer=False, is_writable=False),
+            ]))
+        if account_metas is not None:
+            trx.instructions[-1].keys.extend(account_metas)
+        trx.instructions[-1].keys \
+            .append(AccountMeta(pubkey=PublicKey(sysvarclock), is_signer=False, is_writable=False))
+        return send_transaction(client, trx, self.solana_wallet)
+
+    def create_trx(self, keccak_data, data):
+        print('call_5_create_trx with keccak:', keccak_data.hex(), 'and data:', data.hex())
+        trx = Transaction()
+        trx.add(TransactionInstruction(program_id=keccakprog, data=keccak_data, keys=
+        [
+            AccountMeta(pubkey=PublicKey(keccakprog), is_signer=False, is_writable=False),
+        ]))
+        trx.add(TransactionInstruction(program_id=self.evm_loader.loader_id, data=data, keys=
+        [
+            AccountMeta(pubkey=self.contract_account, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=self.contract_code_account, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=self.solana_ether_caller, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=PublicKey(sysinstruct), is_signer=False, is_writable=False),
+            AccountMeta(pubkey=self.evm_loader.loader_id, is_signer=False, is_writable=False),
+            AccountMeta(pubkey=self.solana_wallet.public_key(), is_signer=False, is_writable=False),
+        ]))
+        return trx
+
+    def send_x_transaction(self, x, input_data, account_metas=None, need_storage=False) -> types.RPCResponse:
+        (from_address, sign, msg) = self.create_instruction_data_from_tx(input_data)
+        keccak_data = make_keccak_instruction_data(1, len(msg), 9 if need_storage else 1)
+        data = x + from_address + sign + msg
+        trx = self.create_trx(keccak_data, data)
+        if need_storage:
+            self.create_storage_account(sign[:8].hex())
+            trx.instructions[-1].keys.insert(0, AccountMeta(pubkey=self.storage, is_signer=False, is_writable=True))
+        if account_metas is not None:
+            trx.instructions[-1].keys.extend(account_metas)
+        trx.instructions[-1].keys \
+            .append(AccountMeta(pubkey=PublicKey(sysvarclock), is_signer=False, is_writable=False))
+        return send_transaction(client, trx, self.solana_wallet)
+
+    def call_5_single(self, input_data, account_metas=None) -> types.RPCResponse:
+        return self.send_x_transaction(bytes.fromhex("05")
+                                       , input_data, account_metas)
+
+    def call_5_begin(self, input_data, account_metas=None, step_count=100) -> types.RPCResponse:
+        return self.send_x_transaction(bytes.fromhex("09") + step_count.to_bytes(8, byteorder='little')
+                                       , input_data, account_metas, need_storage=True)
+
+    def call_5_continue(self, input_data, account_metas=None, step_count=100) -> types.RPCResponse:
+        return self.send_x_transaction(bytes.fromhex("0A") + step_count.to_bytes(8, byteorder='little')
+                                       , input_data, account_metas, need_storage=True)
+
+    def call_5_iterative(self, input_data, account_metas=None, steps=100) -> types.RPCResponse:
+        self.call_5_begin(input_data, account_metas, steps)
+        while True:
+            result = self.call_5_continue(input_data, account_metas, steps)
+            if result['result']['meta']['innerInstructions'] \
+                    and result['result']['meta']['innerInstructions'][0]['instructions']:
+                data = base58.b58decode(result['result']['meta']['innerInstructions'][0]['instructions'][-1]['data'])
+                if data[0] == 6:
+                    self.storage = None
+                    return result
+
+    def call_5(self, input_data, account_metas=None) -> types.RPCResponse:
+        if self.mode is ExecuteMode.SINGLE:
+            return self.call_5_single(input_data, account_metas)
+        if self.mode is ExecuteMode.ITERATIVE:
+            return self.call_5_iterative(input_data, account_metas)
 
 
 class ERC20test(unittest.TestCase):
@@ -219,7 +351,7 @@ class ERC20test(unittest.TestCase):
                                ]))
 
         if mode is Mode.BEGIN:
-            trx.instructions[1].keys.insert(0, AccountMeta(pubkey=storage, is_signer=False, is_writable=True))
+            trx.instructions[-1].keys.insert(0, AccountMeta(pubkey=storage, is_signer=False, is_writable=True))
         if mode is Mode.CONTINUE:
             trx.instructions[0].keys.insert(0, AccountMeta(pubkey=storage, is_signer=False, is_writable=True))
 
@@ -239,6 +371,31 @@ class ERC20test(unittest.TestCase):
         value = data[2:]
         ret = int.from_bytes(value, "big")
         assert 0 != ret, 'erc20_deposit: FAIL'
+        return ret
+
+    def erc20_deposit2(self, ether_contract, payer, amount, balance_erc20, mint_id, receiver_erc20):
+        func_name = abi.function_signature_to_4byte_selector('deposit(uint256,address,uint256,uint256)')
+        data = func_name + bytes.fromhex(
+            base58.b58decode(payer).hex() +
+            str("%024x" % 0) + receiver_erc20.hex() +
+            self.acc.public_key()._key.hex() +
+            "%064x" % amount
+        )
+        result = ether_contract.call_5_iterative(data, [
+            AccountMeta(pubkey=payer, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=balance_erc20, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=mint_id, is_signer=False, is_writable=False),
+            AccountMeta(pubkey=tokenkeg, is_signer=False, is_writable=False),
+        ])
+        print(result)
+        src_data = result['result']['meta']['innerInstructions'][0]['instructions'][-1]['data']
+        data = base58.b58decode(src_data)
+        instruction = data[0]
+        self.assertEqual(instruction, 6)  # 6 means OnReturn
+        self.assertLess(data[1], 0xd0)  # less 0xd0 - success
+        value = data[2:]
+        ret = int.from_bytes(value, "big")
+        assert 0 != ret, 'erc20_deposit2: FAIL'
         return ret
 
     def erc20_withdraw_iterative(self, receiver, amount, erc20, erc20_code, balance_erc20, mint_id):
@@ -270,7 +427,6 @@ class ERC20test(unittest.TestCase):
 
         if mode is Mode.BEGIN:
             storage = self.create_storage_account(sign[:8].hex())
-
 
         trx = Transaction();
         if mode is Mode.BEGIN or mode is Mode.NORMAL:
@@ -351,6 +507,23 @@ class ERC20test(unittest.TestCase):
         balance = int.from_bytes(value, "big")
         return balance
 
+    def erc20_balance2(self, ether_contract):
+        func_name = abi.function_signature_to_4byte_selector('balanceOf(address)')
+        data = func_name + bytes.fromhex(
+            str("%024x" % 0) +
+            ether_contract.ether_caller.hex()
+        )
+        result = ether_contract.call_5(data)
+        print(result)
+        src_data = result['result']['meta']['innerInstructions'][0]['instructions'][-1]['data']
+        data = base58.b58decode(src_data)
+        instruction = data[0]
+        self.assertEqual(instruction, 6)  # 6 means OnReturn
+        self.assertLess(data[1], 0xd0)  # less 0xd0 - success
+        value = data[2:]
+        balance = int.from_bytes(value, "big")
+        return balance
+
     def erc20_transfer(self, erc20, erc20_code, eth_to, amount):
         func_name = abi.function_signature_to_4byte_selector('transfer(address,uint256)')
         input = func_name + bytearray.fromhex(
@@ -407,6 +580,19 @@ class ERC20test(unittest.TestCase):
 
         result = send_transaction(client, trx, self.acc)
         print(result)
+        src_data = result['result']['meta']['innerInstructions'][-1]['instructions'][-1]['data']
+        data = base58.b58decode(src_data)
+        instruction = data[0]
+        self.assertEqual(instruction, 6)  # 6 means OnReturn
+        self.assertLess(data[1], 0xd0)  # less 0xd0 - success
+        value = data[2:]
+        balance_address = base58.b58encode(value)
+        return balance_address
+
+    def erc20_balance_ext2(self, ether_contract):
+        func_name = abi.function_signature_to_4byte_selector('balance_ext()')
+        result = ether_contract.call3_single(func_name)
+        print(result)
         src_data = result['result']['meta']['innerInstructions'][0]['instructions'][0]['data']
         data = base58.b58decode(src_data)
         instruction = data[0]
@@ -432,7 +618,20 @@ class ERC20test(unittest.TestCase):
 
         result = send_transaction(client, trx, self.acc)
         print(result)
-        src_data = result['result']['meta']['innerInstructions'][0]['instructions'][0]['data']
+        src_data = result['result']['meta']['innerInstructions'][-1]['instructions'][-1]['data']
+        data = base58.b58decode(src_data)
+        instruction = data[0]
+        self.assertEqual(instruction, 6)  # 6 means OnReturn
+        self.assertLess(data[1], 0xd0)  # less 0xd0 - success
+        value = data[2:]
+        mint_id = base58.b58encode(value)
+        return mint_id
+
+    def erc20_mint_id2(self, ether_contract):
+        func_name = abi.function_signature_to_4byte_selector('mint_id()')
+        result = ether_contract.call_5(func_name)
+        print(result)
+        src_data = result['result']['meta']['innerInstructions'][-1]['instructions'][-1]['data']
         data = base58.b58decode(src_data)
         instruction = data[0]
         self.assertEqual(instruction, 6)  # 6 means OnReturn
@@ -459,9 +658,13 @@ class ERC20test(unittest.TestCase):
         print("erc20_id_ethereum:", erc20Id_ether.hex())
         print("erc20_code:", erc20_code)
 
+        ether_contract = EtherContract(self.acc, self.loader, self.caller_ether, erc20Id, erc20_code)
+
         assert (self.erc20Id_precalculated == erc20Id)
         assert (balance_erc20 == self.erc20_balance_ext(erc20Id, erc20_code).decode("utf-8"))
+        assert (balance_erc20 == self.erc20_balance_ext2(ether_contract).decode("utf-8"))
         assert (token == self.erc20_mint_id(erc20Id, erc20_code).decode("utf-8"))
+        assert (token == self.erc20_mint_id2(ether_contract).decode("utf-8"))
 
         client_acc = self.createTokenAccount(token)
         print("client_acc:", client_acc)
@@ -471,10 +674,14 @@ class ERC20test(unittest.TestCase):
         assert (self.tokenBalance(client_acc) == mint_amount)
         assert (self.tokenBalance(balance_erc20) == 0)
         assert (self.erc20_balance(erc20Id, erc20_code) == 0)
+        assert (self.erc20_balance2(ether_contract) == 0)
 
         deposit_amount = 1
-        self.erc20_deposit_iterative(client_acc, deposit_amount * (10 ** 9), erc20Id
+        self.erc20_deposit_iterative(client_acc, deposit_amount * (10 ** 9) >> 1, erc20Id
                                      , erc20_code, balance_erc20, token, self.caller_ether)
+        receiver_erc20 = self.caller_ether
+        self.erc20_deposit2(ether_contract, client_acc, deposit_amount * (10 ** 9) >> 1
+                            , balance_erc20, token, receiver_erc20)
         assert (self.tokenBalance(client_acc) == mint_amount - deposit_amount)
         assert (self.tokenBalance(balance_erc20) == deposit_amount)
         assert (self.erc20_balance(erc20Id, erc20_code) == deposit_amount * (10 ** 9))
