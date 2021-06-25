a07bd740683bba994c68c833a1449230a0d04e98
sinev-valentine <sinev-valentine@yandex.ru>
2021-06-25 14:11:06 +0300

    replace create to create2

--- evm_loader/performance/contracts/factory.sol
+++ evm_loader/performance/contracts/factory.sol
@@ -5,16 +5,60 @@ import './ERC20.sol';
 contract Factory{
 
     event Address(address a);
+    event Hash (bytes32 a);
 
-    function create_erc20() public {
+    function get_hash() public {
+        bytes memory bytecode = type(ERC20).creationCode;
+        bytes32 hash = keccak256(abi.encodePacked(bytecode));
+        emit Hash(hash);
+    }
+
+
+    function create_erc20(bytes32 salt) public {
         address addr;
         bytes memory bytecode = type(ERC20).creationCode;
+        // bytes32 salt = keccak256(abi.encodePacked(a));
+
         assembly {
-                addr := create(0, add(bytecode, 0x20), mload(bytecode))
-                if iszero(extcodesize(addr)) {
-                    revert(0, 0)
-                }
+            addr := create2(0, add(bytecode, 32), mload(bytecode), salt)
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
         }
+
+        // assembly {
+                // addr := create(0, add(bytecode, 0x20), mload(bytecode))
+                // if iszero(extcodesize(addr)) {
+                    // revert(0, 0)
+                // }
+        // }
+
         emit Address(addr);
     }
 }
+
+// pragma solidity >0.4.99 <0.6.0;
+
+// contract Factory {
+
+//  event Deployed(address addr, uint256 salt);
+
+//  bytes  code_;
+
+//  function set_code (bytes memory contract_code) public{
+//      code_ = contract_code;
+//  }
+
+//  function deploy(uint256 salt) public {
+//     address addr;
+//     bytes memory code = code_;
+//     assembly {
+//       addr := create2(0, add(code, 0x20), mload(code), salt)
+//       if iszero(extcodesize(addr)) {
+//         revert(0, 0)
+//       }
+//     }
+
+//     emit Deployed(addr, salt);
+//  }
+// }
--- evm_loader/performance/run.py
+++ evm_loader/performance/run.py
@@ -1,31 +1,19 @@
-# from base58 import b58decode
-import sys
 from solana_utils import *
 
 # from ..eth_tx_utils import make_keccak_instruction_data, make_instruction_data_from_tx, Trx
 # from eth_utils import abi
 # from web3.auto import w3
 from eth_keys import keys as eth_keys
-# from web3 import Web3
-# from multiprocessing import Process, Pool
-# from enum import Enum
-import random
+from web3 import Web3
 import argparse
 from eth_utils import abi
 from base58 import b58decode
 
-
-#
-#
-# solana_url = os.environ.get("SOLANA_URL", "http://localhost:8899")
-# http_client = Client(solana_url)
-# # CONTRACTS_DIR = os.environ.get("CONTRACTS_DIR", "evm_loader/")
 # CONTRACTS_DIR = os.environ.get("CONTRACTS_DIR", "contracts/")
 CONTRACTS_DIR = "contracts/"
-# # CONTRACTS_DIR = os.environ.get("CONTRACTS_DIR", "")
 # evm_loader_id = os.environ.get("EVM_LOADER")
 evm_loader_id = "4XS7MwWXNjYjuTKo1KJN92MmSeM4Cw67eihEfcnzUZPP"
-#
+
 # sysinstruct = "Sysvar1nstructions1111111111111111111111111"
 # keccakprog = "KeccakSecp256k11111111111111111111111111111"
 sysvarclock = "SysvarC1ock11111111111111111111111111111111"
@@ -37,7 +25,7 @@ class PerformanceTest():
 
         # wallet = WalletAccount(wallet_path())
         wallet = RandomAccount()
-        tx = client.request_airdrop(wallet.acc.public_key(), 10000 * 10 ** 9)
+        tx = client.request_airdrop(wallet.acc.public_key(), 10000000 * 10 ** 9)
         confirm_transaction(client, tx['result'])
 
         cls.loader = EvmLoader(wallet, evm_loader_id)
@@ -57,27 +45,6 @@ class PerformanceTest():
               "({})".format(bytes(PublicKey(cls.caller)).hex()))
 
 
-    # def test_stages(self, stage: Stage = Stage.create_eth_acc, count = 2):
-    #     if stage == Stage.deploy_erc20:
-    #         addr = deploy_erc20(self.loader, self.caller, 2)
-    #         for i in addr:
-    #             print(i)
-    #         print("deploy complete")
-    #     elif stage == Stage.create_eth_acc:
-    #         sol_addr = []
-    #         eth_addr = []
-    #         for i in range(count):
-    #             eth_addr.append(random.randbytes(20))
-    #             print(eth_addr[-1].hex())
-    #             sol_addr.append(self.loader.createEtherAccount(eth_addr[-1]))
-    #     # elif stage == Stage.create_eth_trx:
-    #     #     for
-#
-# if __name__ == '__main__' and __package__ is None:
-#     from os import sys, path
-#     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-
-
 def check_event(result, factory_eth, erc20_eth):
     assert(result['meta']['err'] == None)
     assert(len(result['meta']['innerInstructions']) == 2)
@@ -94,6 +61,36 @@ def check_event(result, factory_eth, erc20_eth):
     assert(data[61:93] == bytes().fromhex("%024x" % 0)+erc20_eth)  # sum
 
 
+def get_filehash(factory, factory_code, factory_eth):
+    trx = Transaction()
+    trx.add(
+        TransactionInstruction(
+            program_id=evm_loader_id,
+            data=bytearray.fromhex("03") + abi.function_signature_to_4byte_selector('get_hash()'),
+            keys=[
+                AccountMeta(pubkey=factory, is_signer=False, is_writable=True),
+                AccountMeta(pubkey=factory_code, is_signer=False, is_writable=True),
+                AccountMeta(pubkey=instance.acc.public_key(), is_signer=True, is_writable=False),
+                AccountMeta(pubkey=evm_loader_id, is_signer=False, is_writable=False),
+                AccountMeta(pubkey=PublicKey(sysvarclock), is_signer=False, is_writable=False),
+            ]))
+    result = send_transaction(client, trx, instance.acc)
+
+    print (result)
+    assert(result['meta']['err'] == None)
+    assert(len(result['meta']['innerInstructions']) == 1)
+    assert(len(result['meta']['innerInstructions'][0]['instructions']) == 2)
+    data = b58decode(result['meta']['innerInstructions'][0]['instructions'][1]['data'])
+    assert(data[:1] == b'\x06')  #  OnReturn
+    assert(data[1] == 0x11)  # 11 - Machine encountered an explict stop
+
+    data = b58decode(result['meta']['innerInstructions'][0]['instructions'][0]['data'])
+    assert(data[:1] == b'\x07')  # 7 means OnEvent
+    assert(data[1:21] == factory_eth)
+    assert(data[21:29] == bytes().fromhex('%016x' % 1)[::-1])  # topics len
+    filehash = bytedata[61:93]
+
+
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--count', metavar="count of the transaction",  type=int,  help='count transaction (>=1)')
 parser.add_argument('--step', metavar="step of the test", type=str,  help='deploy, create_acc, create_trx, send_trx')
@@ -102,26 +99,47 @@ args = parser.parse_args()
 print(args.count)
 
 if args.step == "deploy":
-    print(args.step)
+    # with open(CONTRACTS_DIR + "ERC20.binary", mode='br') as file:
+    #     content = file.read()
+    #     fileHash = Web3.keccak(content)
+    # factory_eth = bytes.fromhex("BB62F0C4494fdb8576Ae6c94bC4E9F1e6C95b287")
+    #
+    # print (fileHash.hex())
+    # fileHash = bytes.fromhex("12f42fb793a2bea87228e2e2619140f4b1d2e6c34fc5f35ffe1d163ed07c9d23")
+    # salt = bytes().fromhex("%064x" % 5)
+    # erc20_ether = bytes(Web3.keccak(b'\xff' + factory_eth + salt + fileHash)[-20:])
+    # erc20_ether = bytes(Web3.keccak(bytes.fromhex("ff") + factory_eth + salt + fileHash)[-20:])
+    # print ("erc20_ether", erc20_ether.hex())
+    # exit(0)
+
+    # print(args.step)
     instance = PerformanceTest()
     instance.setUpClass()
 
-    print (CONTRACTS_DIR + "factory.binary")
+    # print (CONTRACTS_DIR + "factory.binary")
 
     res = instance.loader.deploy(CONTRACTS_DIR + "Factory.binary", instance.caller)
     (factory, factory_eth, factory_code) = (res['programId'], bytes.fromhex(res['ethereum'][2:]), res['codeId'])
 
+    get_filehash(factory, factory_code);
+    exit(0)
     print("factory", factory)
     print ("factory_eth", factory_eth.hex())
     print("factory_code", factory_code)
-    call_create_erc20 = bytearray.fromhex("03") + abi.function_signature_to_4byte_selector('create_erc20()')
+    # call_create_erc20 = bytearray.fromhex("03") + abi.function_signature_to_4byte_selector('create_erc20()')
+    call_create_erc20 = bytearray.fromhex("03") + abi.function_signature_to_4byte_selector('deploy()')
 
 
+    receipt_list = []
     for  i in range(args.count):
         print (" -- count", i)
         trx_count = getTransactionCount(client, factory)
 
-        erc20_ether = keccak_256(rlp.encode((factory_eth, trx_count))).digest()[-20:]
+        salt = bytes().fromhex("%064x" % i)
+
+        erc20_ether = bytes(Web3.keccak(b'\xff' + factory_eth + salt + fileHash)[-20:])
+        # erc20_ether = keccak_256(rlp.encode((factory_eth, trx_count))).digest()[-20:]
+
         erc20_id = instance.loader.ether2program(erc20_ether)[0]
         seed = b58encode(bytes.fromhex(erc20_ether.hex()))
         erc20_code = accountWithSeed(instance.acc.public_key(), str(seed, 'utf8'), PublicKey(evm_loader_id))
@@ -141,9 +159,6 @@ if args.step == "deploy":
         )
         trx.add(instance.loader.createEtherAccountTrx(erc20_ether, erc20_code)[0])
 
-        # result = send_transaction(client, trx, instance.acc)
-        # print (result)
-        # trx = Transaction()
         trx.add(
             TransactionInstruction(
                 program_id=evm_loader_id,
@@ -157,6 +172,14 @@ if args.step == "deploy":
                     AccountMeta(pubkey=evm_loader_id, is_signer=False, is_writable=False),
                     AccountMeta(pubkey=PublicKey(sysvarclock), is_signer=False, is_writable=False),
             ]))
-        res = send_transaction(client, trx, instance.acc)
-        check_event(res['result'], factory_eth, erc20_ether)
+        # res = send_transaction(client, trx, instance.acc)
+        res = client.send_transaction(trx, instance.acc,
+                                         opts=TxOpts(skip_confirmation=True, preflight_commitment="confirmed"))
 
+        receipt_list.append(res["result"])
+        # check_event(res['result'], factory_eth, erc20_ether)
+
+    for i in range(args.count):
+        confirm_transaction(client, receipt_list[i])
+        result = client.get_confirmed_transaction(receipt_list[i])
+        check_event(receipt_list[i], factory_eth, erc20_ether)
