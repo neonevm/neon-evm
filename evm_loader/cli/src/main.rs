dd193a74c9f945b57c6aeb21c12f4fd52d2ae50e
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2021-06-30 18:58:33 +0300

    Fix clippy (evm #18)

--- evm_loader/cli/src/main.rs
+++ evm_loader/cli/src/main.rs
@@ -416,7 +416,7 @@ fn make_deploy_ethereum_transaction(
         let tx = UnsignedTransaction {
             to: None,
             nonce: trx_count,
-            gas_limit: 9999999.into(),
+            gas_limit: 9_999_999.into(),
             gas_price: 1.into(),
             value: 0.into(),
             data: program_data.to_owned(),
