27d8cba9129646352127f8cb392af6b6469bb44f
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2021-08-16 15:18:14 +0300

    Use rust-web3 version 0.17 (166-faucet)

--- evm_loader/faucet/src/airdrop.rs
+++ evm_loader/faucet/src/airdrop.rs
@@ -1,13 +1,9 @@
 //! Airdrop implementation: calls to the Ethereum network.
 
-use std::sync::Arc;
-
 use color_eyre::Report;
 use tracing::info;
 
-use ethers::prelude::{
-    abigen, Http, LocalWallet, Middleware, Provider, SignerMiddleware, TransactionRequest,
-};
+use web3::contract::{Contract, Options};
 
 use crate::config;
 
@@ -18,76 +14,59 @@ pub struct Airdrop {
     amount: u64,
 }
 
-type Amount = ethers::types::U256;
-type Address = ethers::types::Address;
-
-/// Represents a signer account.
-type Account = SignerMiddleware<Provider<Http>, LocalWallet>;
-
-/// Generates the type representing ERC20 contract.
-impl UniswapV2ERC20<Account> {}
-abigen!(UniswapV2ERC20, "abi/UniswapV2ERC20.abi");
+type Address = web3::types::Address;
 
 /// Processes the aridrop: sends needed transactions into Ethereum.
 pub async fn process(airdrop: Airdrop) -> Result<(), Report> {
     info!("Processing {:?}...", airdrop);
 
-    let admin = derive_address(&config::admin_key())?;
-    use std::convert::TryFrom as _;
-    let provider = Provider::<Http>::try_from(config::ethereum_endpoint())?.with_sender(admin);
-    let admin = Arc::new(import_account(provider.clone(), &config::admin_key())?);
+    let http = web3::transports::Http::new(&config::ethereum_endpoint())?;
+    let web3 = web3::Web3::new(http);
 
+    let recipient = address_from_str(&airdrop.wallet)?;
+    let admin = address_from_str(&config::admin())?;
+
+    info!("Transfer {} -> token A", airdrop.amount);
     let token_a = address_from_str(&config::token_a())?;
-    let token_a = UniswapV2ERC20::new(token_a, admin.clone());
-    let token_b = address_from_str(&config::token_b())?;
-    let token_b = UniswapV2ERC20::new(token_b, admin.clone());
+    let token_a = Contract::from_json(
+        web3.eth(),
+        token_a,
+        include_bytes!("../abi/UniswapV2ERC20.abi"),
+    )?;
 
-    let recipient = &airdrop.wallet;
-    let amount = Amount::from(airdrop.amount);
-
-    info!("Depositing {} -> token A...", amount);
-    let tx = create_transfer_tx(&token_a, recipient, amount).await?;
     info!("Sending transaction for transfer of token A...");
-    let tx = provider.send_transaction(tx, None).await?;
-    info!("Waiting transaction for transfer of token A...");
-    let _receipt = tx.await?;
-    //info!("{:?}", receipt);
+    token_a
+        .call(
+            "transfer",
+            (recipient, airdrop.amount),
+            admin,
+            Options::default(),
+        )
+        .await?;
+
     info!("OK");
 
-    info!("Depositing {} -> token B...", amount);
-    let tx = create_transfer_tx(&token_b, recipient, amount).await?;
+    info!("Transfer {} -> token B", airdrop.amount);
+    let token_b = address_from_str(&config::token_b())?;
+    let token_b = Contract::from_json(
+        web3.eth(),
+        token_b,
+        include_bytes!("../abi/UniswapV2ERC20.abi"),
+    )?;
+
     info!("Sending transaction for transfer of token B...");
-    let tx = provider.send_transaction(tx, None).await?;
-    info!("Waiting transaction for transfer of token B...");
-    let _receipt = tx.await?;
-    //info!("{:?}", receipt);
+    token_b
+        .call(
+            "transfer",
+            (recipient, airdrop.amount),
+            admin,
+            Options::default(),
+        )
+        .await?;
+
     info!("OK");
 
     Ok(())
-}
-
-/// Creates transaction to perform one airdrop operation.
-async fn create_transfer_tx(
-    token: &UniswapV2ERC20<Account>,
-    recipient: &str,
-    amount: Amount,
-) -> Result<TransactionRequest, Report> {
-    let recipient = address_from_str(recipient)?;
-    let call = token.transfer(recipient, amount);
-    Ok(call.tx)
-}
-
-/// Calculates address of given private key.
-fn derive_address(priv_key: &str) -> Result<Address, Report> {
-    let wallet = priv_key.parse::<LocalWallet>()?;
-    Ok(wallet.address())
-}
-
-/// Imports account from it's private key.
-fn import_account(provider: Provider<Http>, priv_key: &str) -> Result<Account, Report> {
-    let wallet = priv_key.parse::<LocalWallet>()?;
-    let account = SignerMiddleware::new(provider, wallet);
-    Ok(account)
 }
 
 /// Converts string representation of address to the H160 hash format.
--- evm_loader/faucet/src/config.rs
+++ evm_loader/faucet/src/config.rs
@@ -43,9 +43,9 @@ pub fn token_b() -> String {
     CONFIG.read().unwrap().token_b.clone()
 }
 
-/// Gets the `admin_key` value.
-pub fn admin_key() -> String {
-    CONFIG.read().unwrap().admin_key.clone()
+/// Gets the `admin` value.
+pub fn admin() -> String {
+    CONFIG.read().unwrap().admin.clone()
 }
 
 /// Represents the main config.
@@ -56,7 +56,7 @@ struct Faucet {
     pub ethereum_endpoint: String,
     pub token_a: String,
     pub token_b: String,
-    pub admin_key: String,
+    pub admin: String,
 }
 
 impl Faucet {
