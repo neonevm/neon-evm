f8f4d3fdf7255c3bf5db1dbf53e136b2831e9c9b
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2021-08-27 16:43:28 +0300

    Delete docker directory (166-faucet)

--- evm_loader/faucet/docker/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# This script is intended to run in a container automatically.
-# It's pointless to try launch it out of that context.
-
-exec faucet --config $HOME/faucet.conf run
--- evm_loader/faucet/docker/logs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Shows logs from the docker container with the faucet service.
-set -e
-
-docker logs faucet
