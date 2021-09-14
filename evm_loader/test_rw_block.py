bc6cc38ddca116b1510bad8e1e9a9590073733fb
sinev-valentine <sinev-valentine@yandex.ru>
2021-09-14 12:56:07 +0300

    test_rw_block update

--- evm_loader/test_rw_block.py
+++ evm_loader/test_rw_block.py
@@ -11,10 +11,8 @@ from eth_utils import abi
 solana_url = os.environ.get("SOLANA_URL", "http://localhost:8899")
 client = Client(solana_url)
 CONTRACTS_DIR = os.environ.get("CONTRACTS_DIR", "evm_loader/")
-# evm_loader_id = os.environ.get("EVM_LOADER")
-evm_loader_id = "Y19LVboXPCTGAE9QqSvnvc58FRQfrRJhBfiLsanKYMM"
-# ETH_TOKEN_MINT_ID: PublicKey = PublicKey(os.environ.get("ETH_TOKEN_MINT"))
-ETH_TOKEN_MINT_ID: PublicKey = PublicKey("HPsV9Deocecw3GeZv1FkAPNCBRfuVyfw9MMwjwRe1xaU")
+evm_loader_id = os.environ.get("EVM_LOADER")
+ETH_TOKEN_MINT_ID: PublicKey = PublicKey(os.environ.get("ETH_TOKEN_MINT"))
 
 class EventTest(unittest.TestCase):
     @classmethod
@@ -22,42 +20,36 @@ class EventTest(unittest.TestCase):
         print("\ntest_event.py setUpClass")
 
         cls.token = SplToken(solana_url)
-        wallet = WalletAccount(wallet_path())
-        # wallet = RandomAccount()
-        cls.loader = EvmLoader(wallet, evm_loader_id)
-        cls.acc = wallet.get_acc()
+        wallet1 = WalletAccount(wallet_path())
 
-        if getBalance(wallet.get_acc().public_key()) == 0:
-            tx = client.request_airdrop(wallet.get_acc().public_key(), 1000000 * 10 ** 9, commitment=Confirmed)
+        cls.loader = EvmLoader(wallet1, evm_loader_id)
+        cls.acc1 = wallet1.get_acc()
+
+        if getBalance(wallet1.get_acc().public_key()) == 0:
+            tx = client.request_airdrop(wallet1.get_acc().public_key(), 1000000 * 10 ** 9, commitment=Confirmed)
             confirm_transaction(client, tx["result"])
 
-        # cls.wallet_token = cls.token.create_token_account(ETH_TOKEN_MINT_ID, owner=wallet.get_path())
-        cls.wallet_token = get_associated_token_address(PublicKey(wallet.get_acc().public_key()), ETH_TOKEN_MINT_ID)
-        cls.token.mint(ETH_TOKEN_MINT_ID, cls.wallet_token, 10000)
+        cls.token.mint(ETH_TOKEN_MINT_ID, get_associated_token_address(PublicKey(wallet1.get_acc().public_key()), ETH_TOKEN_MINT_ID), 10000)
 
 
         # Create ethereum account for user account
-        cls.caller_ether = eth_keys.PrivateKey(cls.acc.secret_key()).public_key.to_canonical_address()
-        (cls.caller, cls.caller_nonce) = cls.loader.ether2program(cls.caller_ether)
-        cls.caller_token = get_associated_token_address(PublicKey(cls.caller), ETH_TOKEN_MINT_ID)
+        cls.caller1_ether = eth_keys.PrivateKey(cls.acc1.secret_key()).public_key.to_canonical_address()
+        (cls.caller1, cls.caller1_nonce) = cls.loader.ether2program(cls.caller1_ether)
+        cls.caller1_token = get_associated_token_address(PublicKey(cls.caller1), ETH_TOKEN_MINT_ID)
 
-        if getBalance(cls.caller) == 0:
-            print("Create caller account...") 
-            _ = cls.loader.createEtherAccount(cls.caller_ether)
+        if getBalance(cls.caller1) == 0:
+            print("Create.caller1 account...")
+            _ = cls.loader.createEtherAccount(cls.caller1_ether)
             print("Done\n")
-        cls.token.transfer(ETH_TOKEN_MINT_ID, 2000, get_associated_token_address(PublicKey(cls.caller), ETH_TOKEN_MINT_ID))
+        cls.token.transfer(ETH_TOKEN_MINT_ID, 2000, get_associated_token_address(PublicKey(cls.caller1), ETH_TOKEN_MINT_ID))
 
-        print('Account:', cls.acc.public_key(), bytes(cls.acc.public_key()).hex())
-        print("Caller:", cls.caller_ether.hex(), cls.caller_nonce, "->", cls.caller,
-              "({})".format(bytes(PublicKey(cls.caller)).hex()))
+        print('Account1:', cls.acc1.public_key(), bytes(cls.acc1.public_key()).hex())
+        print("Caller1:", cls.caller1_ether.hex(), cls.caller1_nonce, "->", cls.caller1,
+              "({})".format(bytes(PublicKey(cls.caller1)).hex()))
 
-        # res = solana_cli().call("config set --keypair " + wallet.get_path() + " -C config.yml")
-
-        # res = cls.loader.deploy(CONTRACTS_DIR+"ReturnsEvents.binary", caller=cls.caller, config="config.yml")
-        # (cls.reId, cls.reId_eth, cls.re_code) = (res['programId'], bytes.fromhex(res['ethereum'][2:]), res['codeId'])
 
         (cls.reId, cls.reId_eth, cls.re_code) = cls.loader.deployChecked(
-            CONTRACTS_DIR+"ReturnsEvents.binary", cls.caller, cls.caller_ether)
+            CONTRACTS_DIR+"ReturnsEvents.binary", cls.caller1, cls.caller1_ether)
         print ('contract', cls.reId)
         print ('contract_eth', cls.reId_eth.hex())
         print ('contract_code', cls.re_code)
@@ -66,31 +58,29 @@ class EventTest(unittest.TestCase):
         cls.collateral_pool_address = create_collateral_pool_address(collateral_pool_index)
         cls.collateral_pool_index_buf = collateral_pool_index.to_bytes(4, 'little')
 
-
-
         # other wallet
-        wallet_other = RandomAccount()
-        cls.acc_other = wallet_other.get_acc()
+        wallet2 = RandomAccount()
+        cls.acc2 = wallet2.get_acc()
 
-        if getBalance(wallet_other.get_acc().public_key()) == 0:
-            tx = client.request_airdrop(wallet_other.get_acc().public_key(), 1000000 * 10 ** 9, commitment=Confirmed)
+        if getBalance(wallet2.get_acc().public_key()) == 0:
+            tx = client.request_airdrop(wallet2.get_acc().public_key(), 1000000 * 10 ** 9, commitment=Confirmed)
             confirm_transaction(client, tx["result"])
 
-        cls.wallet_token = cls.token.create_token_account(ETH_TOKEN_MINT_ID, owner=wallet_other.get_path())
+        cls.wallet2_token = cls.token.create_token_account(ETH_TOKEN_MINT_ID, owner=wallet2.get_path())
 
-        cls.caller_other_ether = eth_keys.PrivateKey(cls.acc_other.secret_key()).public_key.to_canonical_address()
-        (cls.caller_other, cls.caller_other_nonce) = cls.loader.ether2program(cls.caller_other_ether)
+        cls.caller2_ether = eth_keys.PrivateKey(cls.acc2.secret_key()).public_key.to_canonical_address()
+        (cls.caller2, cls.caller2_nonce) = cls.loader.ether2program(cls.caller2_ether)
 
-        if getBalance(cls.caller_other) == 0:
-            print("Create caller account...")
-            _ = cls.loader.createEtherAccount(cls.caller_other_ether)
+        if getBalance(cls.caller2) == 0:
+            print("Create caller2 account...")
+            _ = cls.loader.createEtherAccount(cls.caller2_ether)
             print("Done\n")
 
-        cls.token.transfer(ETH_TOKEN_MINT_ID, 2000, get_associated_token_address(PublicKey(cls.caller_other), ETH_TOKEN_MINT_ID))
+        cls.token.transfer(ETH_TOKEN_MINT_ID, 2000, get_associated_token_address(PublicKey(cls.caller2), ETH_TOKEN_MINT_ID))
 
-        print('Account_other:', cls.acc_other.public_key(), bytes(cls.acc_other.public_key()).hex())
-        print("Caller_other:", cls.caller_other_ether.hex(), cls.caller_other_nonce, "->", cls.caller_other,
-              "({})".format(bytes(PublicKey(cls.caller_other)).hex()))
+        print('Account2:', cls.acc2.public_key(), bytes(cls.acc2.public_key()).hex())
+        print("Caller2:", cls.caller2_ether.hex(), cls.caller2_nonce, "->", cls.caller2,
+              "({})".format(bytes(PublicKey(cls.caller2)).hex()))
 
 
     def sol_instr_09_partial_call(self, storage_account, step_count, evm_instruction, writable_code, acc, caller):
@@ -194,23 +184,21 @@ class EventTest(unittest.TestCase):
         func_name = abi.function_signature_to_4byte_selector('addReturn(uint8,uint8)')
         input = (func_name + bytes.fromhex("%064x" % 0x1) + bytes.fromhex("%064x" % 0x1))
 
-        (from_addr1, sign1,  msg1) = self.get_call_parameters(input, self.acc, self.caller, self.caller_ether)
-        (from_addr2, sign2,  msg2) = self.get_call_parameters(input, self.acc_other, self.caller_other, self.caller_other_ether)
+        (from_addr1, sign1,  msg1) = self.get_call_parameters(input, self.acc1, self.caller1, self.caller1_ether)
+        (from_addr2, sign2,  msg2) = self.get_call_parameters(input, self.acc2, self.caller2, self.caller2_ether)
 
         instruction1 = from_addr1 + sign1 + msg1
         instruction2 = from_addr2 + sign2 + msg2
 
-        storage1 = self.create_storage_account(sign1[:8].hex(), self.acc)
-        storage2 = self.create_storage_account(sign2[1:9].hex(), self.acc_other)
+        storage1 = self.create_storage_account(sign1[:8].hex(), self.acc1)
+        storage2 = self.create_storage_account(sign2[1:9].hex(), self.acc2)
 
-        print("self.acc_other", self.acc_other.public_key())
-        print("self.acc", self.acc.public_key())
-        result = self.call_begin(storage1, 10, msg1, instruction1, False, self.acc, self.caller)
-        result = self.call_begin(storage2, 10, msg2, instruction2, False, self.acc_other, self.caller_other)
-        result = self.call_continue(storage1, 10, False, self.acc, self.caller)
-        result = self.call_continue(storage2, 10, False, self.acc_other, self.caller_other)
-        result1 = self.call_continue(storage1, 1000, False, self.acc, self.caller)
-        result2 = self.call_continue(storage2, 1000, False, self.acc_other, self.caller_other)
+        result = self.call_begin(storage1, 10, msg1, instruction1, False, self.acc1, self.caller1)
+        result = self.call_begin(storage2, 10, msg2, instruction2, False, self.acc2, self.caller2)
+        result = self.call_continue(storage1, 10, False, self.acc1, self.caller1)
+        result = self.call_continue(storage2, 10, False, self.acc2, self.caller2)
+        result1 = self.call_continue(storage1, 1000, False, self.acc1, self.caller1)
+        result2 = self.call_continue(storage2, 1000, False, self.acc2, self.caller2)
 
         self.check_continue_result(result1["result"])
         self.check_continue_result(result2["result"])
