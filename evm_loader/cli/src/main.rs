00ef0c40c1d3213673516e8196e16fe9c1f4de80
Vasiliy Zaznobin <vasiliy.zaznobin@cyber-core.dev>
2021-09-24 00:00:07 +0300

    Introduce command_neon_elf

--- evm_loader/cli/src/main.rs
+++ evm_loader/cli/src/main.rs
@@ -100,7 +100,6 @@ use evm_loader::{
     executor::Machine,
     solana_backend::AccountStorage,
 };
-// use object::Object;
 
 const DATA_CHUNK_SIZE: usize = 229; // Keep program chunks under PACKET_DATA_SIZE
 
@@ -1023,23 +1022,17 @@ fn command_neon_elf(
     _config: &Config,
     program_location: &str,
 ) -> CommandResult {
-    eprintln!("command_neon_elf path={}", &program_location.to_string());
-    // let program_data = read_program_data(program_location)?;
-    // let program_data = &program_data[..];
-    // let elf = object::read::elf::ElfFile::parse(program_data)?;
-    // for val in elf.symbols() {
-    //     println!("Got symbol: {:?}", val);
-    // }
+    let program_data = read_program_data(program_location).unwrap();
+    let program_data = &program_data[..];
+    let elf = goblin::elf::Elf::parse(program_data).expect("Unable to parse ELF file");
+    elf.dynsyms.iter().for_each(|sym| {
+        let name = String::from(&elf.dynstrtab[sym.st_name]);
+        if name.starts_with("NEON")
+        {
+            println!("symbol: name={:?}, sym={:?}", name, sym);
+        }
+    });
     Ok(())
-}
-
-fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
-    let mut f = File::open(&filename).expect("no file found");
-    let metadata = fs::metadata(&filename).expect("unable to read metadata");
-    let mut buffer = vec![0; metadata.len() as usize];
-    f.read(&mut buffer).expect("buffer overflow");
-
-    buffer
 }
 
 fn make_clean_hex(in_str: &str) -> &str {
--- evm_loader/program/src/neon.rs
+++ evm_loader/program/src/neon.rs
@@ -19,3 +19,8 @@ pub static NEON_REVISION: &str = NEON_CONST_REVISION;
 #[no_mangle]
 #[used]
 pub static NEON_SEED_VERSION: u8 = ACCOUNT_SEED_VERSION;
+
+/// NEON BYTE ARRAY
+#[no_mangle]
+#[used]
+pub static NEON_BYTE_ARRAY: &[u8; 4] = &[0x12, 0x34, 0x56, 0x78];
