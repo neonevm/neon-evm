f7242e63e5c8369431d0a16c0867bdc61df8ccc5
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2021-09-23 21:27:49 +0300

    Refactoring (166-faucet)

--- evm_loader/faucet/erc20/ERC20.sol
+++ evm_loader/faucet/erc20/ERC20.sol
@@ -45,7 +45,7 @@ contract TestToken is ERC20Interface, SafeMath {
         symbol = "TST";
         name = "TestToken";
         decimals = 18;
-        _totalSupply = 10000000000000000000000000000;
+        _totalSupply = 100000000000000000000000000000000000000000;
         balances[msg.sender] = _totalSupply;
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
