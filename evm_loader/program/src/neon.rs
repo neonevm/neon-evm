692bb8cb83eb060db54a23bf949b1e25882683de
mich-master <minyas@yandex.ru>
2021-09-29 17:28:48 +0300

    Macro neon_elf added

--- evm_loader/program/src/neon.rs
+++ evm_loader/program/src/neon.rs
@@ -2,54 +2,35 @@
 #![allow(clippy::use_self,clippy::nursery)]
 
 use const_format::formatcp;
-
-const NEON_CONST_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
-const NEON_CONST_REVISION: &str = env!("NEON_REVISION");
 use crate::account_data::ACCOUNT_SEED_VERSION;
-const NEON_CONST_SEED_VERSION: &str = formatcp!("{:?}", ACCOUNT_SEED_VERSION);
 use crate::account_data::ACCOUNT_MAX_SIZE;
-const NEON_CONST_ACCOUNT_MAX_SIZE: &str = formatcp!("{:?}", ACCOUNT_MAX_SIZE);
 
 
-const fn create_byte_array<const SZ: usize>(src: &[u8]) -> [u8; SZ] {
-    let mut array: [u8; SZ] = [0; SZ];
-    let mut i = 0;
-    while i < SZ {
-        array[i as usize] = src[i as usize];
-        i += 1;
+macro_rules! str_as_bytes_len {
+    ($value:expr) => {
+        {
+            $value.as_bytes().len()
+        }
     }
-    array
 }
 
-
-const fn size_of_byte_array(array: &[u8]) -> usize {
-    array.len()
+macro_rules! neon_elf {
+    ($identifier:ident,$value:expr) => {
+        /// NEON DOCS MUST BE HERE
+        pub static $identifier: [u8; str_as_bytes_len!($value)] = 
+            {
+                let mut array: [u8; str_as_bytes_len!($value)] = [0; str_as_bytes_len!($value)];
+                let mut i = 0;
+                while i < str_as_bytes_len!($value) {
+                    array[i] = $value.as_bytes()[i];
+                    i += 1;
+                }
+                array
+            };
+    }
 }
 
-
-const SZ_NEON_PKG_VERSION : usize = size_of_byte_array(NEON_CONST_PKG_VERSION.as_bytes());
-/// NEON VERSION
-#[no_mangle]
-#[used]
-pub static NEON_PKG_VERSION: [u8; SZ_NEON_PKG_VERSION] = create_byte_array::<SZ_NEON_PKG_VERSION>(NEON_CONST_PKG_VERSION.as_bytes());
-
-
-const SZ_NEON_REVISION : usize = size_of_byte_array(NEON_CONST_REVISION.as_bytes());
-/// NEON REVISION
-#[no_mangle]
-#[used]
-pub static NEON_REVISION: [u8; SZ_NEON_REVISION] = create_byte_array::<SZ_NEON_REVISION>(NEON_CONST_REVISION.as_bytes());
-
-
-const SZ_NEON_SEED_VERSION : usize = size_of_byte_array(NEON_CONST_SEED_VERSION.as_bytes());
-/// NEON SEED VERSION
-#[no_mangle]
-#[used]
-pub static NEON_SEED_VERSION: [u8; SZ_NEON_SEED_VERSION] = create_byte_array::<SZ_NEON_SEED_VERSION>(NEON_CONST_SEED_VERSION.as_bytes());
-
-
-const SZ_NEON_ACCOUNT_MAX_SIZE : usize = size_of_byte_array(NEON_CONST_ACCOUNT_MAX_SIZE.as_bytes());
-/// NEON ACCOUNT MAX SIZE
-#[no_mangle]
-#[used]
-pub static NEON_ACCOUNT_MAX_SIZE: [u8; SZ_NEON_ACCOUNT_MAX_SIZE] = create_byte_array::<SZ_NEON_ACCOUNT_MAX_SIZE>(NEON_CONST_ACCOUNT_MAX_SIZE.as_bytes());
+neon_elf!(NEON_PKG_VERSION, env!("CARGO_PKG_VERSION"));
+neon_elf!(NEON_REVISION, env!("NEON_REVISION"));
+neon_elf!(NEON_SEED_VERSION, formatcp!("{:?}", ACCOUNT_SEED_VERSION));
+neon_elf!(NEON_ACCOUNT_MAX_SIZE, formatcp!("{:?}", ACCOUNT_MAX_SIZE));
