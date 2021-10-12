f19e0f572c1c78c27fd8b7820e035669655c635f
mich-master <minyas@yandex.ru>
2021-10-12 11:32:49 +0300

    Authorized Operators 2

--- evm_loader/program/src/operator.rs
+++ evm_loader/program/src/operator.rs
@@ -4,15 +4,6 @@ use solana_program::{
 };
 use crate::error::EvmLoaderError;
 
-// const AUTHORIZED_OPERATOR_LIST: [&'static str; 7] = [
-//     "CVAimMqtcmSUCV4RLZSJAreDpEd7JEZmrvCVj85yaRz1",
-//     "CVAimMqtcmSUCV4RLZSJAreDpEd7JEZmrvCVj85yaRz2",
-//     "CVAimMqtcmSUCV4RLZSJAreDpEd7JEZmrvCVj85yaRz3",
-//     "CVAimMqtcmSUCV4RLZSJAreDpEd7JEZmrvCVj85yaRz4",
-//     "CVAimMqtcmSUCV4RLZSJAreDpEd7JEZmrvCVj85yaRz5",
-//     "CVAimMqtcmSUCV4RLZSJAreDpEd7JEZmrvCVj85yaRz6",
-//     "CVAimMqtcmSUCV4RLZSJAreDpEd7JEZmrvCVj85yaRzZ",
-// ];
 const AUTHORIZED_OPERATOR_LIST: [[u8; 32]; 7] = [
     //  "CVAimMqtcmSUCV4RLZSJAreDpEd7JEZmrvCVj85yaRz1"
     [170, 167, 207, 51, 83, 78, 243, 181, 91, 59, 55, 179, 27, 200, 225, 252, 49, 156, 84, 238, 23, 89, 149, 83, 70, 65, 24, 203, 57, 60, 52, 50],
@@ -35,7 +26,6 @@ pub fn authorized_operator_check(account_info: &AccountInfo) -> Result<(),EvmLoa
     if account_info.is_signer {
         AUTHORIZED_OPERATOR_LIST
             .iter()
-            // .find(|&&item| Pubkey::from_str(item).unwrap() == *account_info.key)
             .find(|&&item| item == account_info.key.to_bytes() )
             .map(|_|())
             .ok_or(EvmLoaderError::UnauthorizedOperator)
