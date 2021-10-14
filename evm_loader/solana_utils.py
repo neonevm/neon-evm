52bba8778b4cf7bf20fc9a4f0d391ee236da8e1d
mich-master <minyas@yandex.ru>
2021-10-14 16:06:57 +0300

    #226 Specified Operator Keypair

--- evm_loader/solana_utils.py
+++ evm_loader/solana_utils.py
@@ -497,7 +497,7 @@ def operator1_keypair_path():
     raise Exception("cannot get keypair path")
 
 def operator2_keypair_path():
-    return "/root/.config/solana/id.json"
+    return "/root/.config/solana/id2.json"
 
 def send_transaction(client, trx, acc):
     result = client.send_transaction(trx, acc, opts=TxOpts(skip_confirmation=True, preflight_commitment="confirmed"))
