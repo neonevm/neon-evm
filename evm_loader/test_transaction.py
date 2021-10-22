51a26c3854d7d5e641a00f84003a9a9898e12c41
Vasiliy Zaznobin <vasiliy.zaznobin@cyber-core.dev>
2021-10-22 10:15:57 +0300

    Fixing test_transaction.py

--- evm_loader/test_transaction.py
+++ evm_loader/test_transaction.py
@@ -372,8 +372,8 @@ class EvmLoaderTestsNewAccount(unittest.TestCase):
         neon_balance_on_5_th_transaction = self.token.balance(self.caller_token_2)
         print("Caller_2 NEON-token balance on sending 5-th transaction:", neon_balance_on_5_th_transaction)
 
-        self.assertEqual((neon_balance_on_start - neon_balance_on_response_1) * 1_000_000_000, 1968)
-        self.assertEqual((neon_balance_on_start - neon_balance_on_response_2) * 1_000_000_000, 1968)
+        self.assertEqual((neon_balance_on_start - neon_balance_on_response_1) * 1_000_000_000, 984)
+        self.assertEqual((neon_balance_on_start - neon_balance_on_response_2) * 1_000_000_000, 1548)
         self.assertEqual((neon_balance_on_start - neon_balance_on_response_3) * 1_000_000_000, EXPECTED_USED_GAS)
         self.assertEqual(neon_balance_on_response_3 - neon_balance_on_5_th_transaction, 0)
 
