cff7880d1de0a7c42160e38e166adc6ca19f16ee
Vasiliy Zaznobin <vasiliy.zaznobin@cyber-core.dev>
2021-11-04 16:10:22 +0300

    Fix NameError: name 'make_instruction_data_from_tx' is not defined

--- evm_loader/test_solidity_precompiles.py
+++ evm_loader/test_solidity_precompiles.py
@@ -2,6 +2,8 @@ import unittest
 from eth_utils import abi
 from base58 import b58decode
 import re
+
+from evm_loader.eth_tx_utils import make_instruction_data_from_tx, make_keccak_instruction_data
 from solana_utils import *
 
 CONTRACTS_DIR = os.environ.get("CONTRACTS_DIR", "evm_loader/")
