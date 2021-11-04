1457386df8bfcada71e626c3a74c87294d993c6e
Vasiliy Zaznobin <vasiliy.zaznobin@cyber-core.dev>
2021-11-04 16:11:07 +0300

    Fix NameError: name 'make_instruction_data_from_tx' is not defined

--- evm_loader/test_delete_account.py
+++ evm_loader/test_delete_account.py
@@ -1,5 +1,7 @@
 import unittest
 from eth_utils import abi
+
+from evm_loader.eth_tx_utils import make_instruction_data_from_tx, make_keccak_instruction_data
 from solana_utils import *
 
 CONTRACTS_DIR = os.environ.get("CONTRACTS_DIR", "evm_loader/")
