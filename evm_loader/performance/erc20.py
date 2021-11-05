da2f44a55175420610c6b75acd19a5bfc1c9dabe
sinev-valentine <sinev-valentine@yandex.ru>
2021-11-05 19:13:07 +0300

    fix last commit

--- evm_loader/performance/erc20.py
+++ evm_loader/performance/erc20.py
@@ -277,7 +277,7 @@ def create_accounts(args):
         if args.type == "spl":
             # spl_token.mint()
             (account_minted, total, event_error, receipt_error, nonce_error, unknown_error, too_small_error) = mint_spl(
-                ether_accounts, instance, arg.key)
+                ether_accounts, instance, args.key)
         elif args.type == "erc20":  # erc20
             # erc20.mint()
             (account_minted, total, event_error, receipt_error, nonce_error, unknown_error, too_small_error) = mint_erc20(
