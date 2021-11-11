4b8697481cf2e2d69624cd459cf5a8a86986e0f6
ivanl <ivanl@cqg.com>
2021-11-11 18:20:00 +0300

    fix token owner

--- deploy-test.sh
+++ deploy-test.sh
@@ -16,11 +16,11 @@ export EVM_LOADER=$(solana address -k evm_loader-keypair.json)
 export $(/opt/neon-cli --evm_loader "$EVM_LOADER" neon-elf-params evm_loader.so)
 
 TOKEN_ACCOUNT=$(spl-token create-account $ETH_TOKEN_MINT --owner $ACCOUNT | grep -Po 'Creating account \K[^\n]*')
-spl-token mint $ETH_TOKEN_MINT 5000 --owner test_token_keypair -- $TOKEN_ACCOUNT
+spl-token mint $ETH_TOKEN_MINT 5000 --owner test_token_owner -- $TOKEN_ACCOUNT
 spl-token balance $ETH_TOKEN_MINT --owner $ACCOUNT
 
 TOKEN_ACCOUNT2=$(spl-token create-account $ETH_TOKEN_MINT --owner $ACCOUNT2 | grep -Po 'Creating account \K[^\n]*')
-spl-token mint $ETH_TOKEN_MINT 5000 --owner test_token_keypair -- $TOKEN_ACCOUNT2
+spl-token mint $ETH_TOKEN_MINT 5000 --owner test_token_owner -- $TOKEN_ACCOUNT2
 spl-token balance $ETH_TOKEN_MINT --owner $ACCOUNT2
 
 python3 -m unittest discover -v -p 'test*.py'
