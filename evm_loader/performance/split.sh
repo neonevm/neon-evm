a16b72859e974239b5739f5a08af04db66b4f6b3
sinev-valentine <sinev-valentine@yandex.ru>
2021-11-19 14:28:55 +0300

    update split.sh

--- evm_loader/performance/split.sh
+++ evm_loader/performance/split.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# $1 - filename: account.json, sender.json or collateral.json
+# $1 - /path/to/account.json (or sender.json or collateral.json)
 # $2 - number of files
 # $3 - destination dir (will be created)
 
+file=$(basename -- $1)
+
 mkdir $3
 
-split -l$((`wc -l < $1`/$2)) $1 $3/$1 -da2
+ split -l$((`wc -l < $1`/$2)) $1 $3/$file -da2
 
 for i in $3/*.json0*
 do
