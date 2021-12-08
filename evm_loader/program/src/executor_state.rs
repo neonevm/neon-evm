90a3d6291c25be52435c9eaf036f7290e22df86a
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2021-12-08 17:07:14 +0500

    avoid buildkite  errors

--- evm_loader/program/src/executor_state.rs
+++ evm_loader/program/src/executor_state.rs
@@ -521,7 +521,7 @@ impl ExecutorSubstate {
     }
 
     pub fn set_code<B: AccountStorage>(&mut self, address: H160, code: &[u8], backend: &B) {
-        self.account_mut(address, backend).valids = Some(Valids::compute(&code));
+        self.account_mut(address, backend).valids = Some(Valids::compute(code));
         self.account_mut(address, backend).code = Some(code.into());
     }
 
