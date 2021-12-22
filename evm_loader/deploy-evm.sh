c3a35f8de7884b1c352ae13a181e42214b20a01e
sinev-valentine <sinev-valentine@yandex.ru>
2021-12-22 17:10:23 +0300

    update deploy-evm.sh

--- evm_loader/deploy-evm.sh
+++ evm_loader/deploy-evm.sh
@@ -16,7 +16,7 @@ if ! solana program deploy --upgrade-authority evm_loader-keypair.json evm_loade
   echo "Failed to deploy evm_loader"
   exit 1
 fi
-
+sleep 30
 
 export ETH_TOKEN_MINT=$(solana address -k neon_token_keypair.json)
 if [ "$ETH_TOKEN_MINT" != "$NEON_TOKEN_MINT" ]; then
