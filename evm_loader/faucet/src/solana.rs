2fc12a61fa2f0c90f724b4aebabba799e67caf35
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-01-17 17:45:20 +0300

    Rollback commitment=finalized (439-faucet-program-error)

--- evm_loader/faucet/src/solana.rs
+++ evm_loader/faucet/src/solana.rs
@@ -147,10 +147,7 @@ pub async fn transfer_token(
         info!("{} Signing transaction...", id);
         tx.try_sign(&[&signer], blockhash)?;
         info!("{} Sending and confirming transaction...", id);
-        client.send_and_confirm_transaction_with_spinner_and_commitment(
-            &tx,
-            CommitmentConfig::finalized(),
-        )?;
+        client.send_and_confirm_transaction(&tx)?;
         info!("{} Transaction is confirmed", id);
 
         Ok(())
