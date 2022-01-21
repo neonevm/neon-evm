74e74bf33cbdfa082507b39da180e354d7dbf842
mich-master <86723787+mich-master@users.noreply.github.com>
2022-01-21 10:47:52 +0300

    482 neon cli refactoring (#483)
    * #482_neon_cli_refactoring
    
    * #482_neon_cli_refactoring
    
    * #482_neon_cli_refactoring

---
+++ evm_loader/cli/src/commands/create_program_address.rs
@@ -0,0 +1,16 @@
+#![allow(clippy::module_name_repetitions)]
+
+use evm::{H160};
+
+use crate::{
+    Config,
+};
+
+pub fn command_create_program_address (
+    config: &Config,
+    ether_address: &H160,
+) {
+    let (solana_address, nonce) = crate::make_solana_program_address(ether_address, &config.evm_loader);
+    println!("{} {}", solana_address, nonce);
+}
+
---
+++ evm_loader/cli/src/commands/mod.rs
@@ -0,0 +1,9 @@
+pub mod cancel_trx;
+pub mod create_ether_account;
+pub mod create_program_address;
+pub mod deploy;
+pub mod emulate;
+pub mod get_ether_account_data;
+pub mod get_neon_elf;
+pub mod get_storage_at;
+pub mod update_valids_table;
