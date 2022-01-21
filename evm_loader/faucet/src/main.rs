2a545c3fb70773fc8495cb9ee79b9508a677c13b
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-01-21 22:15:39 +0300

    Upgrade tracing-subscriber (298-new-instruction-deposit)

--- evm_loader/faucet/src/main.rs
+++ evm_loader/faucet/src/main.rs
@@ -15,7 +15,6 @@ mod version;
 
 use eyre::Result;
 use tracing::info;
-use tracing_subscriber::EnvFilter;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
@@ -26,6 +25,8 @@ async fn main() -> Result<()> {
 
 /// Initializes the logger.
 fn setup() -> Result<()> {
+    use tracing_subscriber::EnvFilter;
+
     if std::env::var("RUST_LIB_BACKTRACE").is_err() {
         std::env::set_var("RUST_LIB_BACKTRACE", "0")
     }
@@ -33,6 +34,7 @@ fn setup() -> Result<()> {
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", "info")
     }
+
     tracing_subscriber::fmt::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
