9a33745da840a0c475dd9be7c1f4a0ce17721ad3
mich-master <minyas@yandex.ru>
2022-01-24 15:47:28 +0300

    #473 Error Message Refactoring

--- evm_loader/cli/src/commands/update_valids_table.rs
+++ evm_loader/cli/src/commands/update_valids_table.rs
@@ -35,7 +35,8 @@ pub fn execute(config: &Config, ether_address: H160) -> NeonCliResult {
         .and_then(|(account,_,_)| {
             info!("account: {:?}", account);
 
-            AccountData::unpack(&account.data).map_err(NeonCliError::from)
+            AccountData::unpack(&account.data)
+                .map_err(NeonCliError::from)
         })
         .and_then(|account_data| {
             info!("account data: {:?}", account_data);
@@ -62,23 +63,33 @@ pub fn execute(config: &Config, ether_address: H160) -> NeonCliResult {
                 );
 
             let finalize_message = Message::new(&[instruction], Some(&config.signer.pubkey()));
-            let (blockhash, fee_calculator) = config.rpc_client.get_recent_blockhash()?;
 
-            check_account_for_fee(
-                &config.rpc_client,
-                &config.signer.pubkey(),
-                &fee_calculator,
-                &finalize_message)?;
+            config.rpc_client.get_recent_blockhash()
+                .map(|(blockhash,fee_calculator)|(finalize_message,blockhash,fee_calculator))
+                .map_err(NeonCliError::from)
+        })
+        .and_then(|(finalize_message,blockhash,fee_calculator)| {
+            info!("fee_calculator: {:?}", fee_calculator);
+
+            check_account_for_fee(&config.rpc_client, &config.signer.pubkey(), &fee_calculator, &finalize_message)
+                .map(|_|(finalize_message,blockhash))
+                .map_err(NeonCliError::from)
+        })
+        .and_then(|(finalize_message,blockhash)| {
+            info!("blockhash: {:?}", blockhash);
 
             let mut finalize_tx = Transaction::new_unsigned(finalize_message);
-            finalize_tx.try_sign(&[&*config.signer], blockhash)?;
-
+            finalize_tx.try_sign(&[&*config.signer], blockhash)
+                .map(|_|finalize_tx)
+                .map_err(NeonCliError::from)
+        })
+        .and_then(|finalize_tx| {
             debug!("signed: {:x?}", finalize_tx);
 
             config.rpc_client
                 .send_and_confirm_transaction_with_spinner(&finalize_tx)
+                .map(|_|())
                 .map_err(NeonCliError::from)
         })
-        .map(|_|())
 }
 
