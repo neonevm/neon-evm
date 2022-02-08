2e302dd853caf23521dfdf0470de9b6e6138eec1
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-02-08 14:29:19 +0300

    struct ReqId (496-actualize-logging-format-on-faucet-A)

--- evm_loader/faucet/src/id.rs
+++ evm_loader/faucet/src/id.rs
@@ -4,7 +4,7 @@ use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use tracing::error;
 
 /// Builds a (hopefully) unique string to mark requests.
-pub fn generate() -> String {
+pub fn generate() -> ReqId {
     let since = match SystemTime::now().duration_since(UNIX_EPOCH) {
         Ok(since) => since,
         Err(err) => {
@@ -13,5 +13,21 @@ pub fn generate() -> String {
         }
     };
     let digest = md5::compute(since.as_nanos().to_string());
-    format!("[{}]", &format!("{:x}", digest)[..7])
+    ReqId {
+        id: format!("[{}]", &format!("{:x}", digest)[..7]),
+    }
+}
+
+/// Represents some context: request id.
+#[derive(Clone)]
+pub struct ReqId {
+    id: String,
+}
+
+use std::fmt;
+
+impl fmt::Display for ReqId {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{\"req_id\": \"{}\"}}", self.id)
+    }
 }
