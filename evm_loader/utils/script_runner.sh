50d62c7b60e8d91b89eaf9faf9b17cd7e99d854c
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-02-09 10:23:42 +0300

    .

--- evm_loader/utils/script_runner.sh
+++ evm_loader/utils/script_runner.sh
@@ -13,20 +13,20 @@ solana_url=http://proxy.night.stand.neontest.xyz/solana
 # https://proxy.devnet.neonlabs.org/solana
 # testnet
 # https://proxy.testnet.neonlabs.org/solana
-evm_loader_id="eeLSJgWzzxrqKv1UxtRVVH8FX3qCQWUs9QuAjJpETGU"
-mint_authority_json_file="../evm_loader_keypair.json"
+evm_loader_id=eeLSJgWzzxrqKv1UxtRVVH8FX3qCQWUs9QuAjJpETGU
+mint_authority_json_file=../evm_loader_keypair.json
 permission="\"allow\"" # "deny"
 grantee="client" # "contract"
 # whitelist 01
-neon_eth_address="0x4cEf46ef9064a6Ec7FfB9a6C905845dc345bfd12"
+neon_eth_address=0x4cEf46ef9064a6Ec7FfB9a6C905845dc345bfd12
 # whitelist 02
 # neon_eth_address=""
 # whitelist 02
 # neon_eth_address=""
-address_list_file="./addresses.txt"
+address_list_file=./addresses.txt
 echo "$solana_url $evm_loader_id $mint_authority_json_file $permission $grantee $neon_eth_address"
 # ./set_single_acct_permission.sh $solana_url $evm_loader_id $mint_authority_json_file $1 $2 $neon_eth_address
-# ./set_many_accts_permission.sh solana_url evm_loader_id mint_authority_json_file $1 $2 address_list_file
+# ./set_many_accts_permission.sh $solana_url $evm_loader_id $mint_authority_json_file $1 $2 $address_list_file
 
 
-./set_single_acct_permission.sh "http://proxy.night.stand.neontest.xyz/solana" "eeLSJgWzzxrqKv1UxtRVVH8FX3qCQWUs9QuAjJpETGU" "../evm_loader_keypair.json" "allow" "client" "0x4cEf46ef9064a6Ec7FfB9a6C905845dc345bfd12"
+set_single_acct_permission.sh "http://proxy.night.stand.neontest.xyz/solana" "eeLSJgWzzxrqKv1UxtRVVH8FX3qCQWUs9QuAjJpETGU" "../evm_loader_keypair.json" "allow" "client" "0x4cEf46ef9064a6Ec7FfB9a6C905845dc345bfd12"
