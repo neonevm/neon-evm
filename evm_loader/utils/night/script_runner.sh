a7c3a15361b20826a3618c932edc2badc488a1b2
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-02-09 11:46:07 +0300

    .

---
+++ evm_loader/utils/night/script_runner.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# param 1 : allow|deny
+# param 2 : client|contract
+
+
+# neon_revision="cf20aac797114fc2741b92a5b0cac424637d68f5"
+solana_url=http://proxy.night.stand.neontest.xyz/solana
+evm_loader_id=eeLSJgWzzxrqKv1UxtRVVH8FX3qCQWUs9QuAjJpETGU
+mint_authority_json_file=../evm_loader_keypair.json
+permission="\"allow\"" # "deny"
+grantee="client" # "contract"
+# whitelist 01
+neon_eth_address=0x4cEf46ef9064a6Ec7FfB9a6C905845dc345bfd12
+# whitelist 02
+# neon_eth_address=""
+# whitelist 02
+# neon_eth_address=""
+address_list_file=./addresses.txt
+echo "$solana_url $evm_loader_id $mint_authority_json_file $permission $grantee $neon_eth_address"
+
+../set_single_acct_permission.sh "$solana_url" "$evm_loader_id" "$mint_authority_json_file" $1 $2 "$neon_eth_address"
