64753449bbcfe0abf62328e9b2d9db5539e27fd8
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-02-11 18:25:48 +0300

    Add test (315-add-deposit-command-in-neon-cli)

---
+++ evm_loader/test_neon_cli.py
@@ -0,0 +1,17 @@
+import unittest
+
+evm_loader_id = os.environ.get("EVM_LOADER")
+
+ETH_TOKEN_MINT_ID: PublicKey = PublicKey(os.environ.get("ETH_TOKEN_MINT"))
+
+class NeonCliTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("\ntest_neon_cli.py setUpClass")
+
+    def test_command_deposit(self):
+        empty_account: bytes = eth_keys.PrivateKey(os.urandom(32)).public_key.to_canonical_address()
+        neon_cli().call("deposit 1 {} --evm_loader {}".format(empty_account, evm_loader_id))
+
+if __name__ == '__main__':
+    unittest.main()
