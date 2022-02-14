fdf8d19fd2b653015b9ae9e1b3930972652268db
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-02-14 10:51:00 +0300

    Bugfix (315-add-deposit-command-in-neon-cli)

--- evm_loader/test_neon_cli.py
+++ evm_loader/test_neon_cli.py
@@ -13,7 +13,7 @@ class NeonCliTest(unittest.TestCase):
         print("\ntest_neon_cli.py setUpClass")
 
     def test_command_deposit(self):
-        empty_account: bytes = eth_keys.PrivateKey(os.urandom(32)).public_key.to_hex()
+        empty_account = eth_keys.PrivateKey(os.urandom(32)).public_key.to_hex()
         neon_cli().call("deposit 10 {} --evm_loader {}".format(empty_account, evm_loader_id))
 
 if __name__ == '__main__':
