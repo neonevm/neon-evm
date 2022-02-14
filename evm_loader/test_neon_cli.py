ba07c6fc93636555fc922d4ffcd87461da4e2f81
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-02-14 18:15:50 +0300

    Developing (315-add-deposit-command-in-neon-cli)

--- evm_loader/test_neon_cli.py
+++ evm_loader/test_neon_cli.py
@@ -12,15 +12,23 @@ class NeonCliTest(unittest.TestCase):
 
     def test_command_deposit(self):
         ether_account = eth_keys.PrivateKey(os.urandom(32)).public_key.to_address()
-        # Place deposit
+        balance_re = re.compile(r"^.*balance:\s+(\d+).*$")
+        # Place deposit to empty account
         neon_cli().call("deposit 10 {} --evm_loader {}".format(ether_account, evm_loader_id))
         # Get account's balance after
         output = neon_cli().call("get-ether-account-data {} --evm_loader {}".format(ether_account, evm_loader_id))
-        balance = re.compile(r".*balance:\s+(\d+).*")
-        balance = balance.match(output)
+        balance = balance_re.match(output)
         self.assertIsNotNone(balance)
         balance = balance.group(1)
         self.assertEqual(balance, '10000000000')
+        # Second deposit (to existing account)
+        neon_cli().call("deposit 10 {} --evm_loader {}".format(ether_account, evm_loader_id))
+        # Get account's balance after
+        output = neon_cli().call("get-ether-account-data {} --evm_loader {}".format(ether_account, evm_loader_id))
+        balance = balance_re.match(output)
+        self.assertIsNotNone(balance)
+        balance = balance.group(1)
+        self.assertEqual(balance, '20000000000')
 
 if __name__ == '__main__':
     unittest.main()
