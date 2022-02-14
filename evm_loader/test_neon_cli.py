6dd8e2df97dd4e827365ee80805e8be75593502f
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-02-14 16:57:35 +0300

    Developing (315-add-deposit-command-in-neon-cli)

--- evm_loader/test_neon_cli.py
+++ evm_loader/test_neon_cli.py
@@ -17,7 +17,7 @@ class NeonCliTest(unittest.TestCase):
         # Get account's balance after
         output = neon_cli().call("get-ether-account-data {} --evm_loader {}".format(ether_account, evm_loader_id))
         balance = re.compile("balance: (.*)")
-        balance = p.match(output).group(1)
+        balance = balance.match(output).group(1)
         self.assertEqual(balance, '10000000000')
 
 if __name__ == '__main__':
