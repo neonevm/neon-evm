69618fd6d62fc729781f9e53a65996d12bdc63d2
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-02-14 19:04:11 +0300

    Developing (315-add-deposit-command-in-neon-cli)

--- evm_loader/test_neon_cli.py
+++ evm_loader/test_neon_cli.py
@@ -12,12 +12,12 @@ class NeonCliTest(unittest.TestCase):
 
     def test_command_deposit(self):
         ether_account = eth_keys.PrivateKey(os.urandom(32)).public_key.to_address()
-        balance_re = re.compile(r"^.*balance:\s+(\d+).*$")
+        balance_re = re.compile(r"^.*balance:\s+(\d+).*$", flags=re.DOTALL)
         # Place deposit to empty account
         neon_cli().call("deposit 10 {} --evm_loader {}".format(ether_account, evm_loader_id))
         # Get account's balance after
         output = neon_cli().call("get-ether-account-data {} --evm_loader {}".format(ether_account, evm_loader_id))
-        balance = balance_re.match(output, flags=re.DOTALL)
+        balance = balance_re.match(output)
         self.assertIsNotNone(balance)
         balance = balance.group(1)
         self.assertEqual(balance, '10000000000')
@@ -25,7 +25,7 @@ class NeonCliTest(unittest.TestCase):
         neon_cli().call("deposit 10 {} --evm_loader {}".format(ether_account, evm_loader_id))
         # Get account's balance after
         output = neon_cli().call("get-ether-account-data {} --evm_loader {}".format(ether_account, evm_loader_id))
-        balance = balance_re.match(output, flags=re.DOTALL)
+        balance = balance_re.match(output)
         self.assertIsNotNone(balance)
         balance = balance.group(1)
         self.assertEqual(balance, '20000000000')
