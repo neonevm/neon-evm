c4e5a056899cd5ce79ff517573ab4c0454775236
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-02-14 11:14:44 +0300

    Bugfix (315-add-deposit-command-in-neon-cli)

--- evm_loader/test_neon_cli.py
+++ evm_loader/test_neon_cli.py
@@ -2,10 +2,7 @@ import unittest
 
 from solana_utils import *
 
-solana_url = os.environ.get("SOLANA_URL", "http://localhost:8899")
 evm_loader_id = os.environ.get("EVM_LOADER")
-
-client = Client(solana_url)
 
 class NeonCliTest(unittest.TestCase):
     @classmethod
@@ -13,7 +10,7 @@ class NeonCliTest(unittest.TestCase):
         print("\ntest_neon_cli.py setUpClass")
 
     def test_command_deposit(self):
-        empty_account = eth_keys.PrivateKey(os.urandom(32)).public_key.to_hex()
+        empty_account = eth_keys.PrivateKey(os.urandom(32)).public_key.to_address()
         neon_cli().call("deposit 10 {} --evm_loader {}".format(empty_account, evm_loader_id))
 
 if __name__ == '__main__':
