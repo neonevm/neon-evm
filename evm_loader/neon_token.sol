7e78681ec79ccf4a94a6393e4aaac38476faccc4
Ivan Loboda <lobodatell@yandex.ru>
2022-02-15 22:41:06 +0300

    fix NeonToken contract

--- evm_loader/neon_token.sol
+++ evm_loader/neon_token.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.5.12;
 contract NeonToken {
     address constant NeonPrecompiled = 0xFF00000000000000000000000000000000000003;
 
-    function withdraw(bytes32 spender) override public payable returns (bool) {
+    function withdraw(bytes32 spender) public payable returns (bool) {
         (bool success, bytes memory returnData) = NeonPrecompiled.delegatecall(abi.encodeWithSignature("withdraw(bytes32)", spender));
         return success;
     }
