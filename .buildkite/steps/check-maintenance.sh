1579d563aa79b387f95dad0785e03e0cf3cfb06b
Vasiliy Zaznobin <vz@neonlabs.org>
2022-02-17 16:59:42 +0300

    Add a new buildkite step: "check maintenance"

--- .buildkite/steps/check-maintenance.sh
+++ .buildkite/steps/check-maintenance.sh
@@ -4,13 +4,16 @@ set -euo pipefail
 REVISION=$(git rev-parse HEAD)
 
 INFRA_REFLECT_FILE="neon-evm.changes"
-INFRA_REFLECT_REPO_PATH="https://github.com/neonlabsorg/neon-infra-inventories/blob/369-calculate-hashes/develop_changes/neon-evm.changes/"
+INFRA_REFLECT_REPO_PATH="https://github.com/neonlabsorg/neon-infra-inventories/blob/develop/develop_changes/"
 MAINTENANCE_FILES="
 ./evm_loader/program/src/config.rs"
 
 echo "MAINTENANCE_FILES=$MAINTENANCE_FILES"
 rm -rf ./neon-infra-inventories/
-git clone -b 369-calculate-hashes https://github.com/neonlabsorg/neon-infra-inventories.git
+git clone -b develop https://github.com/neonlabsorg/neon-infra-inventories.git
+cd ./neon-infra-inventories
+git checkout ${BUILDKITE_BRANCH} || echo "There is no branch with the same name. develop will be used."
+cd ..
 
 git ls-files -s $MAINTENANCE_FILES > "${INFRA_REFLECT_FILE}"".""${REVISION}"
 echo "------ ${INFRA_REFLECT_FILE}:" && cat ./neon-infra-inventories/develop_changes/"${INFRA_REFLECT_FILE}"
