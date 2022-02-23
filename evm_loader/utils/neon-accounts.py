5c58a85da5756018aacc88c79390d4d1596fc979
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-02-23 14:24:38 +0300

    Developing script (313-migrate-accounts-v2)

--- evm_loader/utils/neon-accounts.py
+++ evm_loader/utils/neon-accounts.py
@@ -4,53 +4,63 @@
 ##
 ## Prints accounts owned by the Neon EVM program.
 
-import base64
 import os
+import sys
+import base64
 
 from solana.rpc.api import Client
 
 SOLANA_URL = os.environ.get("SOLANA_URL", "http://solana:8899")
 EVM_LOADER = os.environ.get("EVM_LOADER", "53DfF883gyixYNXnM7s5xhdeyV8mVk9T4i2hGV9vG9io")
 
-def process(pubkey, account):
+def do_migrate(address):
+    os.system("neon-cli-v2 migrate-account " + address
+              + " --url " + SOLANA_URL
+              + " --evm_loader " + EVM_LOADER)
+
+def process(account, command):
     result = (0, 0)
-    print()
-    print("Account:", pubkey)
 
     data = account["data"]
     if data[1] != "base64":
-        print("Non-base64 data format")
+        # print("Non-base64 data format")
         return result
 
     data = base64.b64decode(data[0])
     if len(data) < 21:
-        print("Non-Ethereum account: data size is too small", len(data))
+        # print("Non-Ethereum account: data size is too small", len(data))
         return result
 
     tag = data[0]
     address = "0x" + data[slice(1, 20)].hex()
 
     if tag == 1:
-        print("Ethereum account V1:", address)
+        print("V1:", address)
+        if command == "migrate":
+            do_migrate(address)
         result = (1, 0)
     elif tag == 10:
-        print("Ethereum account V2:", address)
+        print("V2:", address)
         result = (0, 1)
-    else:
-        print("Non-Ethereum account with tag", tag)
 
     return result
 
-### main
+def main():
+    command = ""
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
 
-client = Client(SOLANA_URL)
-response = client.get_program_accounts(EVM_LOADER, encoding="jsonParsed")
+    client = Client(SOLANA_URL)
+    response = client.get_program_accounts(EVM_LOADER, encoding="jsonParsed")
 
-count = (0, 0)
-for account in response["result"]:
-    r = process(account["pubkey"], account["account"])
-    count = (count[0] + r[0], count[1] + r[1])
+    count = (0, 0)
+    for account in response["result"]:
+        r = process(account["account"], command)
+        count = (count[0] + r[0], count[1] + r[1])
 
-print()
-print("Total Ethereum accounts V1:", count[0])
-print("Total Ethereum accounts V2:", count[1])
+    print()
+    print("Total Ethereum accounts V1:", count[0])
+    print("Total Ethereum accounts V2:", count[1])
+
+if __name__ == "__main__":
+    main()
