6fb50dbab0e595517a7a9b2f9dbce2d02253cf1c
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-02-23 16:50:40 +0300

    Developing script (313-migrate-accounts-v2)

--- evm_loader/utils/neon-accounts.py
+++ evm_loader/utils/neon-accounts.py
@@ -2,11 +2,15 @@
 ##
 ## File: neon-accounts.py
 ##
-## Prints accounts owned by the Neon EVM program.
+## Prints Ethereum accounts owned by the Neon EVM program.
+## If executed with 'migrate' argument, performs the
+## migration from V1-accounts to the current version.
 
-import os
-import sys
 import base64
+import io
+import os
+import subprocess
+import sys
 
 from solana.rpc.api import Client
 
@@ -14,9 +18,12 @@ SOLANA_URL = os.environ.get("SOLANA_URL", "http://solana:8899")
 EVM_LOADER = os.environ.get("EVM_LOADER", "53DfF883gyixYNXnM7s5xhdeyV8mVk9T4i2hGV9vG9io")
 
 def do_migrate(address):
-    os.system("neon-cli-v2 migrate-account " + address
-              + " --url " + SOLANA_URL
-              + " --evm_loader " + EVM_LOADER)
+    cli = subprocess.Popen(["neon-cli-v2", "migrate-account", address,
+                            "--url", SOLANA_URL, "--evm_loader", EVM_LOADER],
+                           stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    with io.TextIOWrapper(cli.stdout, encoding="utf-8") as out:
+        for line in out:
+            print(line.strip())
 
 def process(account, command):
     result = (0, 0)
@@ -32,7 +39,7 @@ def process(account, command):
         return result
 
     tag = data[0]
-    address = "0x" + data[slice(1, 20)].hex()
+    address = "0x" + data[slice(1, 21)].hex()
 
     if tag == 1:
         print("V1:", address)
