0aadcbba9dc62dc4e5290b0156e4aaf7571cd821
Dmitriy Borisenko <otselnik@yandex.ru>
2022-02-28 09:51:15 +0300

    sync

--- evm_loader/BlockHashTest.sol
+++ evm_loader/BlockHashTest.sol
@@ -4,12 +4,12 @@ pragma solidity >=0.5.12;
 contract BlockHashTest {
     function getCurrentValues() public view returns (bytes32) {
         uint blockNumber = block.number;
-        bytes32 blockHashNow = blockhash(blockNumber);
+        bytes32 blockHashNow = block.blockhash(blockNumber);
         return blockHashNow;
     }
 
     function getValues(uint number) public view returns (bytes32) {
-        bytes32 blockHash = blockhash(number);
+        bytes32 blockHash = block.blockhash(number);
         return blockHash;
     }
 }
