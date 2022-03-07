117650830126d66fb270b09ce9351f2046331d01
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-03-07 21:43:58 +0500

    Removde extra environment

--- .buildkite/steps/trigger-proxy-step.sh
+++ .buildkite/steps/trigger-proxy-step.sh
@@ -6,12 +6,8 @@ cat << EOF
     build:
       branch: "${PROXY_BRANCH:-develop}"
       env:
-          #TODO: to be dropped away
-          EVM_LOADER_REVISION: "${BUILDKITE_COMMIT}"
-          EVM_LOADER_BRANCH: "${EVM_LOADER_BRANCH}"
-          #-----------------------
           SOLANA_REVISION: "v1.8.12-testnet"
-          EVM_LOADER_FULL_TEST_SUITE: $(buildkite-agent meta-data get "full_test_suite" --default "false")
+          NEON_EVM_FULL_TEST_SUITE: $(buildkite-agent meta-data get "full_test_suite" --default "false")
           NEON_EVM_COMMIT: "${BUILDKITE_COMMIT}"
           NEON_EVM_BRANCH: "${BUILDKITE_BRANCH}"
           NEON_EVM_REPO: "${BUILDKITE_REPO}"
