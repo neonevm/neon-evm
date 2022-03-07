6682d7bb3c06e72e64582d257a2c0d5c81e6bf2d
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-03-07 22:38:10 +0500

    Get rid of git ref-parse to know last commit.

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-REVISION=$(git rev-parse HEAD)
-
-echo "REVISION=$REVISION"
+echo "REVISION=${BUILDKITE_COMMIT}"
 
 set ${SOLANA_REVISION:=v1.8.12-testnet}
 
@@ -11,4 +9,4 @@ set ${SOLANA_REVISION:=v1.8.12-testnet}
 docker pull neonlabsorg/solana:${SOLANA_REVISION}
 echo "SOLANA_REVISION=$SOLANA_REVISION"
 
-docker build --build-arg REVISION=$REVISION --build-arg SOLANA_REVISION=$SOLANA_REVISION -t neonlabsorg/evm_loader:${REVISION} .
+docker build --build-arg REVISION=${BUILDKITE_COMMIT} --build-arg SOLANA_REVISION=$SOLANA_REVISION -t neonlabsorg/evm_loader:${BUILDKITE_COMMIT} .
