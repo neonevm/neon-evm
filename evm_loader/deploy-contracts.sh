6c6565923e4ba970e6729365ec7ed38ec7522e00
Ivan Loboda <lobodatell@yandex.ru>
2022-04-13 15:00:25 +0300

    add comment

--- evm_loader/deploy-contracts.sh
+++ evm_loader/deploy-contracts.sh
@@ -5,6 +5,7 @@ if [ -z "$SOLANA_URL" ]; then
   exit 1
 fi
 
+# NOTE: If you change this key, keep in mind to update also token addresses in tokenlist file
 export DEPLOYER_PRIVATE_KEY='0x4deacb079b4714c38f39508aa8900039f2721ed8686835d43347ba9267da767b'
 export DEPLOYER_PUBLIC_KEY=$(python3 get_deployer_address.py)
 echo "Deployer public key: $DEPLOYER_PUBLIC_KEY"
