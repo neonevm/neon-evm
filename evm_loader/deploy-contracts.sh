31b47578832a5a1941103d6b8cf7d214887a0208
Ivan Loboda <lobodatell@yandex.ru>
2022-04-14 00:40:16 +0300

    Set solana config, add tokens

--- evm_loader/deploy-contracts.sh
+++ evm_loader/deploy-contracts.sh
@@ -5,6 +5,8 @@ if [ -z "$SOLANA_URL" ]; then
   exit 1
 fi
 
+solana config set --url "$SOLANA_URL"
+
 # NOTE: If you change this key, keep in mind to update also token addresses in solidity/tokenlist.json file
 export DEPLOYER_PRIVATE_KEY='0x4deacb079b4714c38f39508aa8900039f2721ed8686835d43347ba9267da767b'
 export DEPLOYER_PUBLIC_KEY=$(python3 get_deployer_address.py)
@@ -18,9 +20,10 @@ neon-cli --evm_loader "$EVM_LOADER" --url "$SOLANA_URL" create-ether-account "$D
 echo "Depositing NEONs to deployer $DEPLOYER_PUBLIC_KEY"
 export ETH_TOKEN_MINT=$(solana address -k /opt/neon_token_keypair.json)
 ACCOUNT=$(solana address --keypair /root/.config/solana/id.json)
-solana --url "$SOLANA_URL" airdrop 1000 "$ACCOUNT"
+echo "Solana account $ACCOUNT"
 TOKEN_ACCOUNT=$(spl-token create-account $ETH_TOKEN_MINT --owner $ACCOUNT | grep -Po 'Creating account \K[^\n]*')
-spl-token mint $ETH_TOKEN_MINT 5000 --owner evm_loader-keypair.json -- $TOKEN_ACCOUNT
+echo "Token accout $TOKEN_ACCOUNT"
+spl-token mint $ETH_TOKEN_MINT 1000 --owner evm_loader-keypair.json -- $TOKEN_ACCOUNT
 spl-token balance $ETH_TOKEN_MINT --owner $ACCOUNT
 neon-cli --url "$SOLANA_URL" deposit 1000 "$DEPLOYER_PUBLIC_KEY" --evm_loader "$EVM_LOADER"
 
