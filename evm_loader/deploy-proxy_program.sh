e732becbd794c88d2a3e61e804f6211ea5c33d6b
Dmitriy Borisenko <otselnik@yandex.ru>
2022-04-21 09:35:22 +0300

    airdrop for deploy

--- evm_loader/deploy-proxy_program.sh
+++ evm_loader/deploy-proxy_program.sh
@@ -7,6 +7,8 @@ fi
 
 solana config set -u "$SOLANA_URL"
 
+solana airdrop 1
+
 export TEST_PROGRAM=$(solana address -k proxy_program-keypair.json)
 
 echo "Deploying proxy_program at address $TEST_PROGRAM..."
