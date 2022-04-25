14430451bb42b6366769f08a8cfbf9beeaeba326
Denis K <100715772+dk-neon@users.noreply.github.com>
2022-04-25 12:37:03 +0500

    Reformatted according to guidelines.

--- evm_loader/program/src/hamt.rs
+++ evm_loader/program/src/hamt.rs
@@ -33,12 +33,11 @@ pub struct Hamt<'a> {
 
 enum ItemType {
     Empty,
-    Item {pos: u32},
-    Array {pos: u32},
+    Item { pos: u32 },
+    Array { pos: u32 },
 }
 
 impl<'a> Hamt<'a> {
-
     /// Hamt constructor
     /// # Errors
     pub fn new(mut data: RefMut<'a, [u8]>) -> Result<Self, ProgramError> {
@@ -54,7 +53,7 @@ impl<'a> Hamt<'a> {
         }
 
         let last_used = u32::from_le_bytes(*last_used_ptr);
-        Ok(Hamt {data, last_used, used: 0, item_count: 0})
+        Ok(Hamt { data, last_used, used: 0, item_count: 0 })
     }
 
     pub fn clear(&mut self) {
@@ -65,15 +64,15 @@ impl<'a> Hamt<'a> {
         let last_used_ptr = array_mut_ref![self.data, 0, 4];
         *last_used_ptr = (header_len as u32).to_le_bytes();
 
-        self.last_used =  u32::from_le_bytes(*last_used_ptr);
+        self.last_used = u32::from_le_bytes(*last_used_ptr);
         self.used = 0;
         self.item_count = 0;
     }
 
     fn allocate_item(&mut self, item_type: u8) -> Result<u32, ProgramError> {
         let free_pos = u32::from(item_type) * (size_of::<u32>() as u32);
-        let size:u32 = match item_type {
-            0 => (256+256)/8,
+        let size: u32 = match item_type {
+            0 => (256 + 256) / 8,
             _ => (4 + u32::from(item_type) * 4),
         };
         if item_type < 32 && item_type > 0 {
@@ -98,8 +97,8 @@ impl<'a> Hamt<'a> {
     fn release_item(&mut self, item_type: u8, item_pos: u32) {
         let free_pos = u32::from(item_type) * (size_of::<u32>() as u32);
         assert!(!(item_type >= 32 || item_type == 0), "Release unreleased items");
-        let size:u32 = match item_type {
-            0 => (256+256)/8,
+        let size: u32 = match item_type {
+            0 => (256 + 256) / 8,
             _ => (4 + u32::from(item_type) * 4),
         };
         self.save_u32(item_pos, self.restore_u32(free_pos));
@@ -110,8 +109,8 @@ impl<'a> Hamt<'a> {
     fn place_item(&mut self, key: U256, value: U256) -> Result<u32, ProgramError> {
         let pos = self.allocate_item(0)?;
         let ptr = array_mut_ref![self.data, pos as usize, 256/8*2];
-        key.to_little_endian(&mut ptr[..256/8]);
-        value.to_little_endian(&mut ptr[256/8..]);
+        key.to_little_endian(&mut ptr[..256 / 8]);
+        value.to_little_endian(&mut ptr[256 / 8..]);
         Ok(pos | 1)
     }
 
@@ -150,8 +149,8 @@ impl<'a> Hamt<'a> {
         let d = self.restore_u32(pos);
         match d {
             0 => ItemType::Empty,
-            n if n & 1 == 1 => ItemType::Item {pos: n & !1},
-            n => ItemType::Array {pos: n & !1}
+            n if n & 1 == 1 => ItemType::Item { pos: n & !1 },
+            n => ItemType::Array { pos: n & !1 }
         }
     }
 
@@ -159,9 +158,9 @@ impl<'a> Hamt<'a> {
     /// # Errors
     pub fn insert(&mut self, key: U256, value: U256) -> Result<(), ProgramError> {
         let (key, tag) = (key >> 5, key.low_u32() & 0b11111);
-        let ptr_pos = 32*4 + tag * 4;
+        let ptr_pos = 32 * 4 + tag * 4;
         let res = self.insert_item(ptr_pos, key, value);
-        if res.is_ok() {self.item_count += 1;};
+        if res.is_ok() { self.item_count += 1; };
         res
     }
 
@@ -171,10 +170,10 @@ impl<'a> Hamt<'a> {
                 let item_pos = self.place_item(key, value)?;
                 self.save_u32(ptr_pos, item_pos);
             },
-            ItemType::Item{pos} => {
+            ItemType::Item { pos } => {
                 let old_key = self.restore_value(pos);
                 if old_key == key {
-                    self.save_value(pos+size_of::<U256>() as u32, &value);
+                    self.save_value(pos + size_of::<U256>() as u32, &value);
                     return Ok(());
                 }
 
@@ -182,50 +181,51 @@ impl<'a> Hamt<'a> {
                 let (mut old_key, mut old_tag) = (old_key >> 5, old_key.low_u32() & 0b11111);
                 let (mut new_key, mut new_tag) = (key >> 5, key.low_u32() & 0b11111);
                 loop {
-                    if old_tag != new_tag {break;}
+                    if old_tag != new_tag { break; }
                     let array_pos = self.allocate_item(1)?;
 
-                    self.save_u32(array_pos, 1<<old_tag);
+                    self.save_u32(array_pos, 1 << old_tag);
                     self.save_u32(ptr_pos, array_pos);
-                    ptr_pos = array_pos+4;
-                    old_tag = old_key.low_u32() & 0b11111; old_key >>= 5;
-                    new_tag = new_key.low_u32() & 0b11111; new_key >>= 5;
+                    ptr_pos = array_pos + 4;
+                    old_tag = old_key.low_u32() & 0b11111;
+                    old_key >>= 5;
+                    new_tag = new_key.low_u32() & 0b11111;
+                    new_key >>= 5;
                 }
 
                 let item_pos = self.place_item(new_key, value)?;
                 self.save_value(pos, &(old_key));
 
                 let tags = (1 << old_tag) | (1 << new_tag);
-                let (item1_pos, item2_pos) = if old_tag < new_tag {(pos|1, item_pos)} else {(item_pos, pos|1)};
+                let (item1_pos, item2_pos) = if old_tag < new_tag { (pos | 1, item_pos) } else { (item_pos, pos | 1) };
 
                 let array_pos = self.place_items2(tags, item1_pos, item2_pos)?;
                 self.save_u32(ptr_pos, array_pos);
 
                 return Ok(());
             },
-            ItemType::Array{pos} => {
+            ItemType::Array { pos } => {
                 let (key, tag) = (key >> 5, key.low_u32() & 0b11111);
                 let tags = self.restore_u32(pos);
                 if tags & (1 << tag) == 0 {
                     // item with this tag doesn't exist in aray -> need resize
                     let total = tags.count_ones();
-                    let shift = (tags & ((1 << tag)-1)).count_ones();
-                    let (before_bytes, after_bytes) = (shift*4, (total-shift)*4);
-                    let array_pos = self.allocate_item((total+1) as u8)?;
+                    let shift = (tags & ((1 << tag) - 1)).count_ones();
+                    let (before_bytes, after_bytes) = (shift * 4, (total - shift) * 4);
+                    let array_pos = self.allocate_item((total + 1) as u8)?;
                     let item_pos = self.place_item(key, value)?;
-                    self.save_u32(array_pos, tags | (1<<tag));
-                    self.data.copy_within((pos+4) as usize..(pos+4+before_bytes) as usize, (array_pos+4) as usize);
-                    self.save_u32(array_pos+4 + before_bytes, item_pos);
-                    self.data.copy_within((pos+4+before_bytes) as usize..(pos+4+before_bytes+after_bytes) as usize, (array_pos+before_bytes+8) as usize);
+                    self.save_u32(array_pos, tags | (1 << tag));
+                    self.data.copy_within((pos + 4) as usize..(pos + 4 + before_bytes) as usize, (array_pos + 4) as usize);
+                    self.save_u32(array_pos + 4 + before_bytes, item_pos);
+                    self.data.copy_within((pos + 4 + before_bytes) as usize..(pos + 4 + before_bytes + after_bytes) as usize, (array_pos + before_bytes + 8) as usize);
                     self.release_item(total as u8, pos);
                     self.save_u32(ptr_pos, array_pos);
                 } else {
                     // item with this tag already exist in array
-                    let shift = (tags & ((1 << tag)-1)).count_ones();
-                    return self.insert_item(pos+4 + shift*4, key, value);
+                    let shift = (tags & ((1 << tag) - 1)).count_ones();
+                    return self.insert_item(pos + 4 + shift * 4, key, value);
                 }
             },
-
         };
         Ok(())
     }
@@ -234,7 +234,7 @@ impl<'a> Hamt<'a> {
     #[must_use]
     pub fn find(&self, key: U256) -> Option<U256> {
         let (key, tag) = (key >> 5, key.low_u32() & 0b11111);
-        let ptr_pos = 32*4 + tag * 4;
+        let ptr_pos = 32 * 4 + tag * 4;
         self.find_item(ptr_pos, key)
     }
 
@@ -243,22 +243,22 @@ impl<'a> Hamt<'a> {
             ItemType::Empty => {
                 None
             },
-            ItemType::Item{pos} => {
+            ItemType::Item { pos } => {
                 let old_key = self.restore_value(pos);
                 if old_key == key {
-                    Some(self.restore_value(pos+size_of::<U256>() as u32))
+                    Some(self.restore_value(pos + size_of::<U256>() as u32))
                 } else {
                     None
                 }
             },
-            ItemType::Array{pos} => {
+            ItemType::Array { pos } => {
                 let (key, tag) = (key >> 5, key.low_u32() & 0b11111);
                 let tags = self.restore_u32(pos);
                 if tags & (1 << tag) == 0 {
                     None
                 } else {
-                    let shift = (tags & ((1 << tag)-1)).count_ones();
-                    self.find_item(pos+4 + shift*4, key)
+                    let shift = (tags & ((1 << tag) - 1)).count_ones();
+                    self.find_item(pos + 4 + shift * 4, key)
                 }
             },
         }
@@ -266,7 +266,7 @@ impl<'a> Hamt<'a> {
 
     /// get last used value
     #[must_use]
-    pub const fn last_used(&self) -> u32{
+    pub const fn last_used(&self) -> u32 {
         self.last_used
     }
 
