72ea3c1224357eb38fb990c31cc78313d76ebf54
Dmitriy Borisenko <otselnik@yandex.ru>
2022-04-28 14:44:13 +0300

    add ability to run multiple trxs

--- evm_loader/proxy_program/src/entrypoint.rs
+++ evm_loader/proxy_program/src/entrypoint.rs
@@ -14,19 +14,40 @@ fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let instruction = Instruction {
-        program_id: *accounts[0].key,
-        accounts: accounts[1..].iter().map(|acc| 
-            if acc.is_writable {
-                AccountMeta::new(*acc.key, acc.is_signer)
-            } else {
-                AccountMeta::new_readonly(*acc.key, acc.is_signer)
-            }
-        ).collect(),
-        data: instruction_data.to_vec()
-    };
-    invoke(
-        &instruction,
-        &accounts[1..],
-    )
+    if instruction_data.len() > 0 && instruction_data[0] > 0xee {
+        for _ in 0..(instruction_data[0] - 0xee) {
+            let instruction = Instruction {
+                program_id: *accounts[0].key,
+                accounts: accounts[1..].iter().map(|acc| 
+                    if acc.is_writable {
+                        AccountMeta::new(*acc.key, acc.is_signer)
+                    } else {
+                        AccountMeta::new_readonly(*acc.key, acc.is_signer)
+                    }
+                ).collect(),
+                data: instruction_data[1..].to_vec()
+            };
+            invoke(
+                &instruction,
+                &accounts[1..],
+            )?;
+        }
+        return  Ok(());
+    } else {
+        let instruction = Instruction {
+            program_id: *accounts[0].key,
+            accounts: accounts[1..].iter().map(|acc| 
+                if acc.is_writable {
+                    AccountMeta::new(*acc.key, acc.is_signer)
+                } else {
+                    AccountMeta::new_readonly(*acc.key, acc.is_signer)
+                }
+            ).collect(),
+            data: instruction_data.to_vec()
+        };
+        invoke(
+            &instruction,
+            &accounts[1..],
+        )
+    }
 }
