7e3a0ed4a9c675513c7138aa9e240c6f865f025e
gigimon <gigimon4ik@gmail.com>
2022-05-17 16:32:18 +0300

    * remove old not actual tests and small refactoring

--- evm_loader/tests/test_resize.py
+++ evm_loader/tests/test_resize.py
@@ -1,64 +1,57 @@
-from solana.transaction import AccountMeta, TransactionInstruction, Transaction
-import unittest
-from base58 import b58encode
+import os
 import web3
-from solana_utils import *
 
-solana_url = os.environ.get("SOLANA_URL", "http://localhost:8899")
-client = Client(solana_url)
-evm_loader_id = os.environ.get("EVM_LOADER")
+from solana.keypair import Keypair
+
+from solana_utils import PublicKey, ACCOUNT_INFO_LAYOUT, EVM_LOADER, EvmLoader, Account, Transaction, get_account_data, \
+    solana_client, AccountInfo, b58encode, ACCOUNT_SEED_VERSION, account_with_seed, create_account_with_seed, TransactionInstruction, \
+    AccountMeta, TransactionWithComputeBudget, OperatorAccount, operator1_keypair_path, send_transaction
 
 
-class ResizeTest(unittest.TestCase):
+def create_resize_transaction(loader: EvmLoader, acc: Keypair, address: str, size: int) -> Transaction:
+    solana_address = PublicKey(loader.ether2program(address)[0])
+    account_data: bytes = get_account_data(solana_client, solana_address, ACCOUNT_INFO_LAYOUT.sizeof())
+    account: AccountInfo = AccountInfo.from_bytes(account_data)
+
+    seed = b58encode(ACCOUNT_SEED_VERSION + os.urandom(20)).decode('utf8')
+    code_account_new = account_with_seed(acc.public_key, seed, PublicKey(EVM_LOADER))
+    minimum_balance = solana_client.get_minimum_balance_for_rent_exemption(size)["result"]
+
+    create_with_seed = create_account_with_seed(acc.public_key, acc.public_key, seed, minimum_balance, size,
+                                                PublicKey(EVM_LOADER))
+    resize = TransactionInstruction(
+        program_id=EVM_LOADER,
+        data=bytearray.fromhex("11") + seed.encode('utf-8'),  # 17- ResizeStorageAccount
+        keys=[
+            AccountMeta(pubkey=solana_address, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=account.code_account, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=code_account_new, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=acc.public_key, is_signer=True, is_writable=False)
+        ]
+    )
+
+    trx = TransactionWithComputeBudget()
+    trx.add(create_with_seed)
+    trx.add(resize)
+
+    return trx
+
+
+class TestResize:
+    loader: EvmLoader
+    account: Keypair
+
     @classmethod
-    def setUpClass(cls):
-        print("\ntest_resize.py setUpClass")
-
+    def setup_class(cls):
         wallet = OperatorAccount(operator1_keypair_path())
-        cls.loader = EvmLoader(wallet, evm_loader_id)
-        cls.acc = wallet.get_acc()
-
-
-    def sol_instr_17_resize(self, address, size) -> Transaction:
-        solana_address = PublicKey(self.loader.ether2program(address)[0])
-        account_data: bytes = getAccountData(client, solana_address, ACCOUNT_INFO_LAYOUT.sizeof())
-        account: AccountInfo = AccountInfo.frombytes(account_data)
-
-        seed = b58encode(ACCOUNT_SEED_VERSION + os.urandom(20)).decode('utf8')
-        code_account_new = accountWithSeed(self.acc.public_key(), seed, PublicKey(evm_loader_id))
-        minimum_balance = client.get_minimum_balance_for_rent_exemption(size)["result"]
-
-        create_with_seed = createAccountWithSeed(self.acc.public_key(), self.acc.public_key(), seed, minimum_balance, size, PublicKey(evm_loader_id))
-        resize = TransactionInstruction(
-            program_id=evm_loader_id,
-            data=bytearray.fromhex("11") + seed.encode('utf-8'),  # 17- ResizeStorageAccount
-            keys=[
-                AccountMeta(pubkey=solana_address, is_signer=False, is_writable=True),
-                AccountMeta(pubkey=account.code_account, is_signer=False, is_writable=True),
-                AccountMeta(pubkey=code_account_new, is_signer=False, is_writable=True),
-                AccountMeta(pubkey=self.acc.public_key(), is_signer=True, is_writable=False)
-            ]
-        )
-
-        trx = TransactionWithComputeBudget()
-        trx.add(create_with_seed)
-        trx.add(resize)
-
-        return trx
-
-
+        cls.loader = EvmLoader(wallet, EVM_LOADER)
+        cls.account = Keypair.from_secret_key(wallet.get_acc().secret_key())
 
     def test_resize(self):
         account = web3.Account.create()
-        self.loader.createEtherAccount(account.address)
+        self.loader.create_ether_account(account.address)
 
         for size in range(1*1024*1024, 10*1024*1024 + 1, 1*1024*1024):
-            print("resize account, size = ", size)
-
-            resize_trx = self.sol_instr_17_resize(account.address, size)
-            result = send_transaction(client, resize_trx, self.acc)
-            print(result)
- 
-
-if __name__ == '__main__':
-    unittest.main()
+            resize_trx = create_resize_transaction(self.loader, self.account, account.address, size)
+            result = send_transaction(solana_client, resize_trx, self.account)
+            assert result["status"] == {"OK": None}
