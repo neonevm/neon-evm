a42677108849d44ce6274ba483465f80e9c77f8f
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-05-17 21:55:06 +0300

    Test (29-diamond-event)

--- evm_loader/program/src/account/program.rs
+++ evm_loader/program/src/account/program.rs
@@ -105,7 +105,7 @@ impl<'a> Neon<'a> {
         #[allow(clippy::cast_possible_truncation)]
         let mnemonic = [b'L', b'O', b'G', b'0' + log.topics.len() as u8];
         fields.push(&mnemonic);
-        fields.push(log.address.as_bytes());
+        //fields.push(log.address.as_bytes());
         //for topic in &log.topics {
         //    fields.push(topic.as_bytes());
         //}
