fc197f4d955969d224114f40f7b7686fa3262b5d
gigimon <gigimon4ik@gmail.com>
2022-05-20 14:35:09 +0300

    * fix scripts

--- evm_loader/collateral_pool_generator.py
+++ evm_loader/collateral_pool_generator.py
@@ -1,7 +1,9 @@
-from solana.publickey import PublicKey
-from solana.transaction import Transaction
-from solana_utils import *
 import sys
+
+from solana.publickey import PublicKey
+from solana.rpc.commitment import Confirmed
+from tests.solana_utils import OperatorAccount, account_with_seed, EVM_LOADER, solana_client, TransactionWithComputeBudget, send_transaction, create_account_with_seed, get_solana_balance
+
 
 wallet = OperatorAccount(sys.argv[1]).get_acc()
 collateral_pool_base = wallet.public_key()
@@ -9,13 +11,13 @@ print(collateral_pool_base)
 for collateral_pool_index in range(0, 10):
     COLLATERAL_SEED_PREFIX = "collateral_seed_"
     seed = COLLATERAL_SEED_PREFIX + str(collateral_pool_index)
-    collateral_pool_address = accountWithSeed(PublicKey(collateral_pool_base), seed, PublicKey(EVM_LOADER))
+    collateral_pool_address = account_with_seed(PublicKey(collateral_pool_base), seed, PublicKey(EVM_LOADER))
     print("Collateral pool address: ", collateral_pool_address)
-    if getBalance(collateral_pool_address) == 0:
+    if get_solana_balance(collateral_pool_address) == 0:
         print("Creating...")
-        minimum_balance = client.get_minimum_balance_for_rent_exemption(0, commitment=Confirmed)["result"]
+        minimum_balance = solana_client.get_minimum_balance_for_rent_exemption(0, commitment=Confirmed)["result"]
         trx = TransactionWithComputeBudget()
-        trx.add(createAccountWithSeed(wallet.public_key(), PublicKey(collateral_pool_base), seed, minimum_balance, 0, PublicKey(EVM_LOADER)))
-        result = send_transaction(client, trx, wallet)
+        trx.add(create_account_with_seed(wallet.public_key(), PublicKey(collateral_pool_base), seed, minimum_balance, 0, PublicKey(EVM_LOADER)))
+        result = send_transaction(solana_client, trx, wallet)
         print(result)
 print(collateral_pool_base)
