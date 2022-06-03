1af8e1e4274fe13f56f106501e556c1f9076505e
Denis K <100715772+dk-neon@users.noreply.github.com>
2022-06-03 18:46:45 +0500

    Implemented utility for storage migration from v1 to v2 (#679)
    * Implemented WriteValueToDistributedStorage instruction.
    
    * Small improvement.
    
    * Added data validation.
    
    * Small improvement.
    
    * Improved instruction data unpacking.
    
    * Implemented contract conversion instruction and tests.
    
    * Implemented migration utility.
    
    * Fixed bugs found during review.
    
    * Implemented account data reallocation, if needed.
    
    * Removed iteration over HAMT items.
    
    * Fixed bugs found during review, made optimizations.
    
    * Reallocate even when account data is shrinked.
    
    * Used single creation of contract_v1 in conversion.
    
    * Update storage account balance to both sides.
    
    * Implemented own JSON-RPC client. Improved logging.
    
    * Fixed bugs. Optimized.
    
    * Improved transaction batching.
    
    * Ignore V1 accounts.
    
    * Several fixes.
    
    * Fixed script.
    
    * Rewritten write_value_to_distributed_storage instruction in order to do not use AccountStorage. Fixed error in instruction serialization.
    
    * Used fixed storage account location in instruction.
    
    * Fixed lamports transferring from account being converted.
    
    * Fixed bug in is_all_data_written(), added additional logging.
    
    * Added skip_errors flag.
    
    * Used macro for public key parsing.

--- evm_loader/program/src/entrypoint.rs
+++ evm_loader/program/src/entrypoint.rs
@@ -77,6 +77,12 @@ fn process_instruction<'a>(
         EvmInstruction::ExecuteTrxFromAccountDataIterativeOrContinueNoChainId => {
             instruction::transaction_step_from_account_no_chainid::process(program_id, accounts, instruction)
         },
+        EvmInstruction::WriteValueToDistributedStorage => {
+            instruction::storage_to_v2::write_value_to_distributed_storage::process(program_id, accounts, instruction)
+        },
+        EvmInstruction::ConvertDataAccountFromV1ToV2 => {
+            instruction::storage_to_v2::convert_data_account_from_v1_to_v2::process(program_id, accounts, instruction)
+        },
         EvmInstruction::OnReturn | EvmInstruction::OnEvent => { Ok(()) },
         _ => Err!(ProgramError::InvalidInstructionData; "Invalid instruction"),
     };
