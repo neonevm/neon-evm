bb780c06f3c3f561ae90368e4cbf90d370561821
sinev-valentine <sinev-valentine@yandex.ru>
2022-06-24 13:08:41 +0300

    update

--- evm_loader/program/src/lib.rs
+++ evm_loader/program/src/lib.rs
@@ -1,7 +1,7 @@
 //! # Neon EVM
 //!
 //! Neon EVM is an implementation of Ethereum Virtual Machine on Solana.
-#![deny(warnings)]
+// #![deny(warnings)]
 #![deny(clippy::all, clippy::pedantic, clippy::nursery)]
 #![allow(clippy::module_name_repetitions, clippy::missing_const_for_fn, clippy::use_self)]
 #![allow(missing_docs, clippy::missing_panics_doc, clippy::missing_errors_doc)]
