b913e1b2256ff776861c75e21852bd29d497d06b
Semen Medvedev <sm@neonlabs.org>
2022-08-03 18:29:04 +0700

    NDEV-367 Cleanup commented code

--- evm_loader/program/src/instruction/collect_treasury.rs
+++ evm_loader/program/src/instruction/collect_treasury.rs
@@ -8,12 +8,6 @@ use solana_program::{
     rent::Rent,
 };
 
-//struct Accounts<'a> {
-//    main_treasury: MainTreasury<'a>,
-//    treasury: Treasury<'a>,
-//    //system_program: program::System<'a>,
-//}
-
 pub fn process<'a>(program_id: &'a Pubkey, accounts: &'a [AccountInfo<'a>], instruction: &[u8]) -> ProgramResult {
     solana_program::msg!("Instruction: Collect treasury");
 
@@ -21,7 +15,6 @@ pub fn process<'a>(program_id: &'a Pubkey, accounts: &'a [AccountInfo<'a>], inst
 
     let main_treasury = MainTreasury::from_account(&accounts[0])?;
     let treasury = Treasury::from_account(program_id, treasury_index, &accounts[1])?;
-        //system_program: program::System::from_account(&accounts[2])?,
 
     let rent = Rent::get()?;
     let minimal_balance_for_rent_exempt = rent.minimum_balance(treasury.data_len());
@@ -31,8 +24,5 @@ pub fn process<'a>(program_id: &'a Pubkey, accounts: &'a [AccountInfo<'a>], inst
     **main_treasury.lamports.borrow_mut() = main_treasury.lamports().checked_add(available_lamports)
         .ok_or_else(|| E!(ProgramError::InvalidArgument; "Main treasary lamports overflow"))?;
 
-    //accounts.system_program.transfer(&accounts.treasury, &accounts.main_treasury, available_lamports)?;
-
-    //super::transaction::do_continue(step_count, accounts, storage, &mut account_storage)
     Ok(())
 }
