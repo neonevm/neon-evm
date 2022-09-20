703673677d7d509b9d10a44c5fa83b13fd565002
sinev-valentine <sinev-valentine@yandex.ru>
2022-09-20 12:37:36 +0300

    remove NeonERC20Wrapper

--- evm_loader/solidity/scripts/deploy.js
+++ evm_loader/solidity/scripts/deploy.js
@@ -44,47 +44,6 @@ async function deployQueryAccount() {
   console.log("QueryAccount library address is: ", queryAccount.address);
 }
 
-async function deployERC20(token_list_file) {
-  fs.readFile(token_list_file, 'utf8' , async (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-
-    const chainId = hre.network.config.chainId;
-    let token_list = JSON.parse(data);
-    const NeonERC20Wrapper = await hre.ethers.getContractFactory("NeonERC20Wrapper");
-
-    for (let spl_token of token_list.tokens) {
-      if (chainId != spl_token.chainId) {
-        continue;
-      }
-
-      if (!createSplToken(spl_token)) {
-        continue;
-      }
-
-      console.log(`Deploying wrapper for SPL token ${spl_token.name} (${spl_token.symbol})`);
-      const new_wrapper = await NeonERC20Wrapper.deploy(
-          spl_token.name,
-          spl_token.symbol,
-          base58_to_binary(spl_token.address_spl));
-
-      await new_wrapper.deployed();
-      console.log(`   Wrapper deployed: ${new_wrapper.address}`);
-      spl_token.address = new_wrapper.address;
-    }
-
-    fs.writeFile(
-        token_list_file,
-        JSON.stringify(token_list, null, ' '),
-        function(err) {
-          if (err) {
-            console.log(err);
-          }
-        });
-  });
-}
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -92,7 +51,6 @@ async function main() {
 
   await deployNeon();
   await deployQueryAccount();
-  await deployERC20('./tokenlist.json');
 }
 
 main().catch((error) => {
