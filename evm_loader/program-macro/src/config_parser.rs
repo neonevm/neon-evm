8457eb533828029247eecbe19bb0a203a85317b7
Yuri Albuquerque <yuridenommus@gmail.com>
2022-10-20 09:55:51 -0300

    Automatically generating variables from the common TOML file

--- evm_loader/program-macro/src/config_parser.rs
+++ evm_loader/program-macro/src/config_parser.rs
@@ -30,18 +30,3 @@ pub struct AccountWhitelists {
     pub neon_minimal_client_allowance_balance: String,
     pub neon_minimal_contract_allowance_balance: String,
 }
-
-#[derive(Deserialize)]
-pub struct CommonConfig {
-    pub payment_to_treasure: u64,
-    pub payment_to_deposit: u64,
-    pub operator_priority_slots: u64,
-    pub holder_msg_size: u64,
-    pub compute_budget_units: u32,
-    pub compute_budget_heap_frame: u32,
-    pub request_units_additional_fee: u64,
-    pub gas_limit_multiplier_no_chainid: u32,
-    pub storage_entries_in_contract_account: u32,
-    pub evm_steps_min: u64,
-    pub evm_steps_last_iteration_max: u64,
-}
