f495682b21eff4d77cf92489985f06cc6dabdf81
Kristina Nikolaeva <112946046+kristinaNikolaevaa@users.noreply.github.com>
2023-01-20 12:41:12 +0400

    tests stabilization (#848)
    * tests stabilization
    
    * Update github_api_client.py
    
    * Update test_cli.py
    
    * Update test_transaction_step_from_account.py
    
    * fix

--- evm_loader/tests/test_cli.py
+++ evm_loader/tests/test_cli.py
@@ -22,7 +22,7 @@ def gen_hash_of_block(size: int) -> str:
     """Generates a block hash of the given size"""
     try:
         block_hash = hex(int.from_bytes(os.urandom(size), "big"))
-        if bytes.fromhex(block_hash[2:]):
+        if bytes.fromhex(block_hash[2:]) or len(block_hash[2:]) != size * 2:
             return block_hash
     except ValueError:
         return gen_hash_of_block(size)
