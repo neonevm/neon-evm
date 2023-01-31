3d909733cd64acb4d2442f2ae30cb4514ef03253
Denis Avvakumov <denisavvakumov@gmail.com>
2023-01-31 21:06:18 +0400

    NDEV-1142: Implement get_block_time, get_latest_blockhash

--- evm_loader/cli/src/rpc/db_clickhouse.rs
+++ evm_loader/cli/src/rpc/db_clickhouse.rs
@@ -1,28 +1,34 @@
 use super::Rpc;
-use clickhouse::Client;
-use std::any::Any;
-use crate::rpc::TxParams;
-use crate::rpc::ClientResult;
-use crate::rpc::Hash;
-use crate::rpc::CommitmentConfig;
-use crate::rpc::Signature;
-use crate::rpc::RpcSendTransactionConfig;
-use crate::rpc::Transaction;
-use crate::rpc::EncodedConfirmedTransactionWithStatusMeta;
-use crate::rpc::RpcTransactionConfig;
-use crate::rpc::TransactionStatus;
-use crate::rpc::RpcResult;
-use crate::rpc::Slot;
-use crate::rpc::UnixTimestamp;
-use crate::rpc::EncodedConfirmedBlock;
-use crate::rpc::Pubkey;
+use crate::rpc::block;
+use crate::rpc::e;
 use crate::rpc::Account;
+use crate::rpc::ClientResult;
+use crate::rpc::CommitmentConfig;
+use crate::rpc::EncodedConfirmedBlock;
+use crate::rpc::EncodedConfirmedTransactionWithStatusMeta;
+use crate::rpc::Hash;
+use crate::rpc::Pubkey;
+use crate::rpc::RpcResult;
+use crate::rpc::RpcSendTransactionConfig;
+use crate::rpc::RpcTransactionConfig;
+use crate::rpc::Signature;
+use crate::rpc::Slot;
+use crate::rpc::Transaction;
+use crate::rpc::TransactionStatus;
+use crate::rpc::TxParams;
+use crate::rpc::UnixTimestamp;
+
+use clickhouse::Client;
+use solana_client::client_error::ClientError;
+use solana_client::client_error::ClientErrorKind;
+use std::any::Any;
 
 #[allow(dead_code)]
 struct ClickHouseClient {
     client: Client,
 }
 
+#[allow(dead_code)]
 impl ClickHouseClient {
     pub fn _new(
         server_url: &str,
@@ -39,6 +45,42 @@ impl ClickHouseClient {
         };
 
         ClickHouseClient { client }
+    }
+
+    pub fn get_block_time_(&self, slot: Slot) -> ClientResult<UnixTimestamp> {
+        block(|| async {
+            let query = "SELECT JSONExtractInt(notify_block_json, 'block_time') FROM events.notify_block_local WHERE (slot = toUInt64(?))";
+            self.client
+                .query(query)
+                .bind(slot)
+                .fetch_one::<UnixTimestamp>()
+                .await
+                .map_err(|e| e!("Failed to get block time, error: {}", e))
+        })
+    }
+
+    pub fn get_block_(&self, _slot: Slot) -> ClientResult<EncodedConfirmedBlock> {
+        let _ = self;
+        todo!()
+    }
+
+    pub fn get_latest_blockhash_(&self) -> ClientResult<Hash> {
+        block(|| async {
+            let query =
+                "SELECT hash FROM events.notify_block_local ORDER BY retrieved_time DESC LIMIT 1";
+            let hash_string = self
+                .client
+                .query(query)
+                .fetch_one::<String>()
+                .await
+                .map_err(|e| e!("Failed to get the latest blockhash, error: {}", e))?;
+
+            let hash_vec = bs58::decode(hash_string)
+                .into_vec()
+                .map_err(|e| e!("Failed to decode the latest blockhash, error: {}", e))?;
+
+            Ok(Hash::new(hash_vec.as_slice()))
+        })
     }
 }
 
@@ -72,16 +114,16 @@ impl Rpc for ClickHouseClient {
         todo!()
     }
 
-    fn get_block(&self, _slot: Slot) -> ClientResult<EncodedConfirmedBlock> {
-        todo!()
+    fn get_block(&self, slot: Slot) -> ClientResult<EncodedConfirmedBlock> {
+        self.get_block_(slot)
     }
 
-    fn get_block_time(&self, _slot: Slot) -> ClientResult<UnixTimestamp> {
-        todo!()
+    fn get_block_time(&self, slot: Slot) -> ClientResult<UnixTimestamp> {
+        self.get_block_time_(slot)
     }
 
     fn get_latest_blockhash(&self) -> ClientResult<Hash> {
-        todo!()
+        self.get_latest_blockhash_()
     }
 
     fn get_minimum_balance_for_rent_exemption(&self, _data_len: usize) -> ClientResult<u64> {
