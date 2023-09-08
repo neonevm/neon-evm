b5663a322227c7a874a27bf8c5b191e766911650
Stanislav Tkach <stanislav.tkach@gmail.com>
2023-09-08 17:06:52 +0200

    Update the 'ethnum' crate version and remove serialization workaround (#187)

--- evm_loader/program/src/executor/action.rs
+++ evm_loader/program/src/executor/action.rs
@@ -17,17 +17,17 @@ pub enum Action {
     NeonTransfer {
         source: Address,
         target: Address,
-        #[serde(with = "serde_u256")]
+        #[serde(with = "ethnum::serde::bytes::le")]
         value: U256,
     },
     NeonWithdraw {
         source: Address,
-        #[serde(with = "serde_u256")]
+        #[serde(with = "ethnum::serde::bytes::le")]
         value: U256,
     },
     EvmSetStorage {
         address: Address,
-        #[serde(with = "serde_u256")]
+        #[serde(with = "ethnum::serde::bytes::le")]
         index: U256,
         #[serde(with = "serde_bytes_32")]
         value: [u8; 32],
@@ -89,63 +89,6 @@ mod serde_bytes_32 {
         }
 
         deserializer.deserialize_bytes(BytesVisitor)
-    }
-}
-
-mod serde_u256 {
-    use ethnum::U256;
-    use serde::{
-        de::{self, SeqAccess, Visitor},
-        Deserializer, Serializer,
-    };
-
-    pub fn serialize<S>(value: &U256, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_bytes(&value.to_le_bytes())
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<U256, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_bytes(U256Visitor {})
-    }
-
-    struct U256Visitor {}
-
-    impl<'de> Visitor<'de> for U256Visitor {
-        type Value = U256;
-
-        fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-            f.write_str("32 bytes in little endian")
-        }
-
-        fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
-        where
-            E: de::Error,
-        {
-            let bytes = v
-                .try_into()
-                .map_err(|_| E::invalid_length(v.len(), &self))?;
-            Ok(U256::from_le_bytes(bytes))
-        }
-
-        fn visit_seq<S>(self, mut seq: S) -> Result<Self::Value, S::Error>
-        where
-            S: SeqAccess<'de>,
-        {
-            let mut bytes = Vec::with_capacity(32);
-            while let Some(b) = seq.next_element()? {
-                bytes.push(b);
-            }
-            Ok(U256::from_le_bytes(
-                bytes
-                    .try_into()
-                    .map_err(|_| de::Error::custom("Invalid U256 value"))?,
-            ))
-        }
     }
 }
 
