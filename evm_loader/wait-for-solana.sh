5ba148b75afe4f8bd0db4e9bc709162b72c23979
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2023-09-12 13:09:42 +0300

    NDEV-2081: More useful CI logs (#191)

--- evm_loader/wait-for-solana.sh
+++ evm_loader/wait-for-solana.sh
@@ -9,12 +9,14 @@ else
   WAIT_TIME=$1
   echo "Waiting $WAIT_TIME seconds for solana cluster to be available at $SOLANA_URL"
   for i in $(seq 1 $WAIT_TIME); do
-      if solana -u $SOLANA_URL cluster-version >/dev/null 2>&1; then exit 0; fi
+      echo "Try solana cluster-version count=$i"
+      if solana -u $SOLANA_URL cluster-version; then
+        echo "Executed solana cluster-version successfully after count=$i"
+        exit 0
+      fi
       sleep 1
   done
 fi
 
 echo "unable to connect to solana cluster $SOLANA_URL"
 exit 1
-
-
