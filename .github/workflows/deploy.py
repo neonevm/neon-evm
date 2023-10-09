ba57c4a751077b760ef87ec465e7dd2e8fab9f03
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2023-10-09 12:31:28 +0300

    NDEV-2292: Upgrade Solana to v1.16.15 (#211)

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -30,8 +30,8 @@ ERR_MSG_TPL = {
 DOCKER_USER = os.environ.get("DHUBU")
 DOCKER_PASSWORD = os.environ.get("DHUBP")
 IMAGE_NAME = 'neonlabsorg/evm_loader'
-SOLANA_NODE_VERSION = 'v1.16.14'
-SOLANA_BPF_VERSION = 'v1.16.14'
+SOLANA_NODE_VERSION = 'v1.16.15'
+SOLANA_BPF_VERSION = 'v1.16.15'
 
 VERSION_BRANCH_TEMPLATE = r"[vt]{1}\d{1,2}\.\d{1,2}\.x.*"
 docker_client = docker.APIClient()
