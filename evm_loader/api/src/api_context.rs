a04990ff0220dedd6a9691c10df3541451e35bc1
Stanislav Tkach <stanislav.tkach@gmail.com>
2023-10-12 16:32:23 +0200

    Add 'write version' to EmulateRequestModel (#212)
    * Add 'write version' to EmulateRequestModel

--- evm_loader/api/src/api_context.rs
+++ evm_loader/api/src/api_context.rs
@@ -6,19 +6,21 @@ use std::sync::Arc;
 pub async fn build_rpc_client(
     state: &NeonApiState,
     slot: Option<u64>,
+    tx_index_in_block: Option<u64>,
 ) -> Result<Arc<dyn rpc::Rpc>, NeonError> {
     if let Some(slot) = slot {
-        return build_call_db_client(state, slot).await;
+        build_call_db_client(state, slot, tx_index_in_block).await
+    } else {
+        Ok(state.rpc_client.clone())
     }
-
-    Ok(state.rpc_client.clone())
 }
 
 pub async fn build_call_db_client(
     state: &NeonApiState,
     slot: u64,
+    tx_index_in_block: Option<u64>,
 ) -> Result<Arc<dyn rpc::Rpc>, NeonError> {
     Ok(Arc::new(
-        CallDbClient::new(state.tracer_db.clone(), slot).await?,
+        CallDbClient::new(state.tracer_db.clone(), slot, tx_index_in_block).await?,
     ))
 }
